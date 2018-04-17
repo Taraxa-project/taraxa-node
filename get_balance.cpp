5e75cc5a92bc80e9d56994101ef84a33ae617790
Ilja Honkonen <ilja.honkonen@fmi.fi>
2018-04-17 19:10:53 +0300

    Process all transactions in balance tracking program.

--- get_balance.cpp
+++ get_balance.cpp
@@ -15,6 +15,7 @@ Copyright 2018 Ilja Honkonen
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <cryptopp/blake2.h>
+#include <cryptopp/integer.h>
 #include <rapidjson/document.h>
 #include <rapidjson/error/en.h>
 
@@ -186,19 +187,207 @@ int main(int argc, char* argv[]) {
 			continue;
 		}
 
+		if (accounts.count(transaction.pubkey_hex) == 0) {
+			std::cerr << __FILE__ "(" << __LINE__ << ")" << std::endl;
+			return EXIT_FAILURE;
+		}
+
 		auto& account = accounts.at(transaction.pubkey_hex);
 		if (verbose) {
-			std::cout << "Setting initial balance for account " << account.address_hex
+			std::cout << "Setting initial balance of account " << account.address_hex
 				<< " to " << transaction.new_balance_hex << std::endl;
 		}
 		account.balance_hex = transaction.new_balance_hex;
 
 		processed_transactions[transaction.hash_hex] = transaction;
+
+		if (transaction.next_hex.size() == 0) {
+			continue;
+		}
 	}
 
 	/*
 	Iterate through transactions to discover requested balance(s).
 	*/
 
+	std::set<std::string> transactions_to_process;
+	for (const auto& item: transactions) {
+		const auto& transaction = item.second;
+		if (processed_transactions.count(transaction.hash_hex) == 0) {
+			transactions_to_process.insert(transaction.hash_hex);
+		}
+	}
+
+	while (transactions_to_process.size() > 0) {
+
+		// find next one
+		const auto
+			&transaction_to_process = [&](){
+				for (const auto& transaction_hex: transactions_to_process) {
+					if (transactions.count(transaction_hex) == 0) {
+						throw std::runtime_error(__FILE__ "(" + std::to_string(__LINE__) + ")");
+					}
+
+					const auto& transaction = transactions.at(transaction_hex);
+
+					// must process previous first
+					if (
+						transaction.previous_hex != "0000000000000000000000000000000000000000000000000000000000000000"
+						and processed_transactions.count(transaction.previous_hex) == 0
+					) {
+						if (verbose) {
+							std::cout << "Skipping " << transaction.hash_hex
+								<< " because previous not processed" << std::endl;
+						}
+						continue;
+					}
+
+					// receive
+					if (transaction.send_hex.size() > 0) {
+
+						// process send first
+						if (processed_transactions.count(transaction.send_hex) == 0) {
+							if (verbose) {
+								std::cout << "Skipping " << transaction.hash_hex
+									<< " because matching send not processed" << std::endl;
+							}
+							continue;
+						}
+
+						return transaction;
+
+					// send
+					} else {
+						return transaction;
+					}
+				}
+
+				throw std::runtime_error("Couldn't process any transaction");
+			}();
+
+		if (verbose) {
+			std::cout << "Processing " << transaction_to_process.hash_hex << std::endl;
+		}
+
+		CryptoPP::Integer old_balance_sender, new_balance_sender, old_balance_receiver;
+
+		// receive
+		if (transaction_to_process.send_hex.size() > 0) {
+
+			if (processed_transactions.count(transaction_to_process.send_hex) == 0) {
+				std::cerr << "Send " << transaction_to_process.send_hex
+					<< " not processed for receive" << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			const auto& send = processed_transactions.at(transaction_to_process.send_hex);
+
+			if (processed_transactions.count(send.previous_hex) == 0) {
+				std::cerr << "Prior transaction " << send.previous_hex
+					<< " for send " << send.hash_hex << " not processed" << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			CryptoPP::Integer
+				old_balance_sender(
+					processed_transactions
+						.at(send.previous_hex)
+						.new_balance_hex.c_str()
+				),
+				new_balance_sender(send.new_balance_hex.c_str()),
+				old_balance_receiver(0l);
+
+			if (transaction_to_process.previous_hex != "0000000000000000000000000000000000000000000000000000000000000000") {
+				old_balance_receiver
+					= CryptoPP::Integer(
+						transactions
+							.at(transaction_to_process.previous_hex)
+							.new_balance_hex.c_str()
+					);
+			}
+
+			CryptoPP::Integer new_balance_receiver = old_balance_receiver + old_balance_sender - new_balance_sender;
+
+			if (new_balance_receiver <= old_balance_receiver) {
+				std::cerr << "New balance of receiver must be larger than old:\n"
+					<< "Old transaction: " << transaction_to_process.previous_hex
+					<< ", balance: " << old_balance_receiver
+					<< "\nNew transaction: " << transaction_to_process.hash_hex
+					<< ", balance: " << new_balance_receiver
+					<< std::endl;
+				return EXIT_FAILURE;
+			}
+
+			if (verbose) {
+				std::cout << "Balance before receive: " << old_balance_receiver
+					<< ", balance after receive: " << new_balance_receiver << std::endl;
+			}
+
+			if (accounts.count(transaction_to_process.pubkey_hex) == 0) {
+				std::cerr << "No account for receive transaction "
+					<< transaction_to_process.hash_hex << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			auto& receiver = accounts.at(transaction_to_process.pubkey_hex);
+
+			std::string new_balance_receiver_bin(8, 0);
+			new_balance_receiver.Encode(
+				reinterpret_cast<CryptoPP::byte*>(const_cast<char*>(new_balance_receiver_bin.data())),
+				8
+			);
+
+			receiver.balance_hex = taraxa::bin2hex(new_balance_receiver_bin);
+
+		// send
+		} else {
+
+			CryptoPP::Integer
+				old_balance(
+					transactions
+						.at(transaction_to_process.previous_hex)
+						.new_balance_hex.c_str()
+				),
+				new_balance(transaction_to_process.new_balance_hex.c_str());
+
+			if (new_balance >= old_balance) {
+				std::cerr << "New balance of sender must be smaller than old:\n"
+					<< "Old transaction: " << transaction_to_process.previous_hex
+					<< ", new-balance: " << old_balance
+					<< "\nNew transaction: " << transaction_to_process.hash_hex
+					<< ", new-balance: " << new_balance
+					<< std::endl;
+				return EXIT_FAILURE;
+			}
+
+			if (verbose) {
+				std::cout << "Balance before send: " << old_balance
+					<< ", balance after send: " << new_balance << std::endl;
+			}
+
+			if (accounts.count(transaction_to_process.pubkey_hex) == 0) {
+				std::cerr << "No account for send transaction "
+					<< transaction_to_process.hash_hex << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			auto& sender = accounts.at(transaction_to_process.pubkey_hex);
+			sender.balance_hex = transaction_to_process.new_balance_hex;
+		}
+
+		processed_transactions[transaction_to_process.hash_hex] = transaction_to_process;
+		transactions_to_process.erase(transaction_to_process.hash_hex);
+		if (transaction_to_process.next_hex.size() > 0) {
+			transactions_to_process.insert(transaction_to_process.next_hex);
+		}
+	}
+
+	std::cout << "Final balances:" << std::endl;
+	for (const auto& item: accounts) {
+		const auto& account = item.second;
+		std::cout << account.address_hex.substr(0, 5) << "..."
+			<< account.address_hex.substr(account.address_hex.size() - 6, 5)
+			<< ": " << account.balance_hex << std::endl;
+	}
 	return EXIT_SUCCESS;
 }
