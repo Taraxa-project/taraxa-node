0ac2391352a234ff62f332924625146c72fee457
Ilja Honkonen <ilja.honkonen@fmi.fi>
2018-11-08 16:13:25 +0200

    Add sortition program and finish instructions.

--- documentation/algorand_sortition.py
+++ documentation/algorand_sortition.py
@@ -5,12 +5,23 @@
 from argparse import ArgumentParser
 from functools import total_ordering
 
+def hex_int(i):
+	return int(i, 16)
+
 parser = ArgumentParser(description = 'Prints whether user was selected')
+parser.add_argument('--verbose', default = False, action = 'store_true')
 parser.add_argument('--balance', type = int, nargs = '+', default = [1], help = "Users' number of coins (decimal)")
-parser.add_argument('--account', type = int, nargs = '+', default = [0], help = "Users' accounts (decimal, 0 <= account < 2**BITS)")
-parser.add_argument('--hash', type = int, nargs = '+', default = [1], help = "Users' hashes produced from verifiable random function (decimal, 0 <= hash < 2**BITS)")
+parser.add_argument('--account', type = hex_int, nargs = '+', default = [0], help = "Users' accounts (hex, 0 <= account < 2**BITS)")
+parser.add_argument('--hash', type = hex_int, nargs = '+', default = [1], help = "Users' hashes produced from verifiable random function (hex, 0 <= hash < 2**BITS)")
 parser.add_argument('--bits', type = int, default = 1, help = 'Maximum number of bits in hash')
 args = parser.parse_args()
+
+
+if len(args.account) != len(args.balance):
+	exit('Number of accounts and balances must match.')
+if len(args.account) != len(args.hash):
+	exit('Number of accounts and hashes must match.')
+
 
 # order coins by account (hash)
 @total_ordering
@@ -30,12 +41,7 @@ class Account:
 	def __lt__(self, other):
 		return self.id_ < other.id_
 	def __repr__(self):
-		return '(' + str(self.id_) + ': ' + str(self.balance_) + ')'
-
-if len(args.account) != len(args.balance):
-	exit('Number of accounts and balances must match.')
-if len(args.account) != len(args.hash):
-	exit('Number of accounts and hashes must match.')
+		return '({:x}: {:d})'.format(self.id_, self.balance_)
 
 accounts = sorted(
 	[Account(args.account[i], args.balance[i], args.hash[i])
@@ -57,10 +63,12 @@ for i in range(len(accounts)):
 	# hash range
 	hash_f = accounts[i].hash_ / 2**args.bits
 	if hash_f >= start_f and hash_f < end_f:
-		print('Selecting account', accounts[i].id_, 'because', start_f, '<=', hash_f, '<', end_f)
+		if args.verbose:
+			print('Selecting account {:x} because'.format(accounts[i].id_), start_f, '<=', hash_f, '<', end_f)
 		selected.append(i)
 	else:
-		print('Not selecting account', accounts[i].id_, 'because', start_f, '<=', hash_f, '<', end_f, 'is false')
+		if args.verbose:
+			print('Not selecting account {:x} because'.format(accounts[i].id_), start_f, '<=', hash_f, '<', end_f, 'is false')
 	start = end
 
 print('Selected accounts:', end = ' ')
