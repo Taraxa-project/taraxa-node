de864f641a65b6c6cb96105d103f2c069d97285b
Chia-Chun Lin <starrynightglider@gmail.com>
2018-12-17 15:42:49 -0800

    boost library, collect tips and main chain

--- dag.cpp
+++ dag.cpp
@@ -3,34 +3,22 @@
  * @Author: Chia-Chun Lin 
  * @Date: 2018-12-14 10:59:17 
  * @Last Modified by: Chia-Chun Lin
- * @Last Modified time: 2018-12-14 18:28:54
+ * @Last Modified time: 2018-12-17 15:39:18
  */
  
 #include "dag.hpp"
-namespace taraxa {
-		
-template < typename Vertex, typename Graph >
-void MyVisitor::discover_vertex(Vertex u, const Graph & g) const { 
-	std::cout << "At " << u << std::endl; 
-}
-template < typename Vertex, typename Graph >
-void MyVisitor::finish_vertex(Vertex u, const Graph & g) const { 
-	std::cout << "Exit " << u << std::endl; 
-}
-template < typename Edge, typename Graph >
-void MyVisitor::examine_edge(Edge e, const Graph& g) const { 
-	std::cout << "Examining edges " << e << std::endl;
-}
+#include <tuple>
+#include <fstream>
 
-// class MyVisitor : public boost::default_dfs_visitor
-// {
-// public:
-//   void discover_vertex(Dag::vertex_t v, const Dag::graph_t & g) const
-//   {
-//     //std::cout << v << std::endl;
-//     return;
-//   }
-// };
+namespace taraxa {
+
+Dag::Dag(){
+	genesis_ = addVertex("0000");
+}
+Dag::~Dag(){}
+Dag::graph_t Dag::getGraph() { return graph_;}
+
+Dag::vertex_t Dag::getGenesis() {return genesis_;}
 
 Dag::vertex_t Dag::addVertex(blk_hash_t hash){
 	vertex_t ret=boost::add_vertex(hash, graph_);
@@ -41,29 +29,81 @@ Dag::edge_t Dag::addEdge(Dag::vertex_t v1, Dag::vertex_t v2){
 	assert(ret.second);
 	return ret.first;
 }
-void Dag::printAllVertexes() const{
-	auto index_map = boost::get(boost::vertex_index, graph_);
-	for (auto vp= boost::vertices(graph_); vp.first!=vp.second; ++vp.first){
-		std::cout<<"Vertex("<<index_map[*vp.first]<<")\n";
+
+void Dag::collectLeafVertexes(std::vector<vertex_t> &leaves) const{
+	leaves.clear();
+	vertex_iter_t s, e;
+	// iterator all vertex
+	for (std::tie(s,e) = boost::vertices(graph_); s!=e; ++s){
+		// if out-degree zero, leaf node
+		if (boost::out_degree(*s, graph_)==0){
+			leaves.push_back(*s);
+		}
+	}
+
+}
+void Dag::collectCriticalPath(std::vector<vertex_t> &critical_path) const{
+	critical_path.clear();
+	// starting from genesis node
+	vertex_t current = genesis_;
+	while (boost::out_degree(current, graph_)){
+		vertex_adj_iter_t critical;
+		std::tie(critical, std::ignore) = boost::adjacent_vertices(current, graph_);
+		vertex_adj_iter_t s, e;
+		for (std::tie(s,e) = boost::adjacent_vertices(current, graph_); s!=e; s++){
+			size_t current_vertex_deg = boost::out_degree(*s, graph_);
+			size_t critical_vertex_deg = boost::out_degree(*critical, graph_);
+			if (current_vertex_deg >critical_vertex_deg){
+				critical = s;
+			}
+			// tie breaker, choose the one has smaller hash
+			else if (current_vertex_deg == critical_vertex_deg){
+				// this is a const member function, need const type
+				vertex_name_map_const_t name_map = boost::get(boost::vertex_name, graph_);
+				if (name_map[*s]<name_map[*critical]){
+					critical = s;
+				}
+			}
+		}
+		critical_path.push_back(*critical);
+		current = *critical;
 	}
 }
 
-void Dag::dfs() const{
-	MyVisitor vis;
-	auto index_map = boost::get(boost::vertex_index, graph_);
-	boost::depth_first_search(graph_, boost::visitor(vis));
+void Dag::drawGraph(std::string filename) const{
+	std::ofstream outfile(filename.c_str());
+	auto name_map = boost::get(boost::vertex_name, graph_);
+	boost::write_graphviz(outfile, graph_, make_label_writer(name_map)); 
+	std::cout<<"Dot file "<<filename<< " generated!"<<std::endl;
+	std::cout<<"Use \"dot -Tps <dot file> -o <ps file>\" to generate ps file"<<std::endl;
 }
 
 
 }
 
 int main(int argc, char *argv[]){
+	
 	taraxa::Dag graph;
-
-	auto v0= graph.addVertex("0");
+	auto v0= graph.addVertex("10");
 	auto v1= graph.addVertex("1");
+	auto v2= graph.addVertex("2888");
+	auto v3= graph.addVertex("35");
+	auto genesis = graph.getGenesis();
+	graph.addEdge(genesis, v0);
 	graph.addEdge(v0, v1);
-	graph.printAllVertexes();
-	graph.dfs();
+	graph.addEdge(v0, v2);
+	graph.addEdge(v2, v3);
+	std::vector<taraxa::Dag::vertex_t> leaves, critical;
+	graph.collectLeafVertexes(leaves);
+	graph.collectCriticalPath(critical);
+	auto v = boost::get(boost::vertex_name, graph.getGraph());
+
+	
+	for (auto e:leaves)
+		std::cout<<"Leaf "<<e<<" "<<v[e]<<std::endl; 
+	for (auto e:critical)
+		std::cout<<"Critical "<<e<<" "<<v[e]<<std::endl; 
+
+	graph.drawGraph("draw.dot");
 	return 0;
 }
--- dag.hpp
+++ dag.hpp
@@ -3,58 +3,87 @@
  * @Author: Chia-Chun Lin 
  * @Date: 2018-12-14 13:23:51 
  * @Last Modified by: Chia-Chun Lin
- * @Last Modified time: 2018-12-14 18:29:38
+ * @Last Modified time: 2018-12-17 15:37:22
  */
  
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
+#include <boost/graph/properties.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/graphviz.hpp>
 #include "types.hpp"
 
 namespace taraxa{
 
-class MyVisitor : public boost::default_dfs_visitor 
-{ 
-public: 
-	template < typename Vertex, typename Graph >
-	void discover_vertex(Vertex u, const Graph & g) const;
-	template < typename Vertex, typename Graph >
-	void finish_vertex(Vertex u, const Graph & g) const;
-	template < typename Edge, typename Graph >
-	void examine_edge(Edge e, const Graph& g) const; 
-};
+
 
 class Dag {
 public:
 	// properties
-	typedef boost::property<boost::vertex_index_t, blk_hash_t, 
-		boost::property<boost::vertex_out_degree_t, uint32_t> > vertex_property_t;
+	// typedef boost::property<boost::vertex_name_t, blk_hash_t, 
+	// 	boost::property<boost::vertex_out_degree_t, uint32_t> > vertex_property_t;
+	typedef boost::property<boost::vertex_name_t, blk_hash_t> vertex_property_t;
 	typedef boost::property<boost::edge_index_t, uint64_t> edge_property_t;
 	
 	// graph def
-	typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, 
+	typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, 
 		vertex_property_t, edge_property_t> graph_t;
 	typedef boost::graph_traits<graph_t>::vertex_descriptor vertex_t;
 	typedef boost::graph_traits<graph_t>::edge_descriptor edge_t;
 	typedef boost::graph_traits<graph_t>::vertex_iterator vertex_iter_t;
 	typedef boost::graph_traits<graph_t>::edge_iterator edge_iter_t;
+	typedef boost::graph_traits<graph_t>::adjacency_iterator vertex_adj_iter_t;
+
 
 	// property_map
-	typedef boost::property_map<graph_t, boost::vertex_index_t>::type vertex_index_map_t;
-	typedef boost::property_map<graph_t, boost::vertex_out_degree_t>::type vertex_out_degree_map_t;
+	typedef boost::property_map<graph_t, boost::vertex_name_t>::const_type vertex_name_map_const_t;
+	typedef boost::property_map<graph_t, boost::vertex_name_t>::type vertex_name_map_t;
+	typedef boost::property_map<graph_t, boost::edge_index_t>::const_type edge_index_map_const_t;
 	typedef boost::property_map<graph_t, boost::edge_index_t>::type edge_index_map_t;
-
+	Dag ();
+	~Dag ();
+	graph_t getGraph();
+	vertex_t getGenesis();
 	vertex_t addVertex(blk_hash_t v);
 	edge_t   addEdge(vertex_t from, vertex_t to);
 	void dfs() const;
-	void printAllVertexes() const;
+	void collectLeafVertexes(std::vector<vertex_t> &leaves) const;
+	void collectCriticalPath(std::vector<vertex_t> &leaves) const;
+	void drawGraph(std::string filename) const;
+	
+	class LeafVisitor2: public boost::default_bfs_visitor{
+	public:
+		LeafVisitor2(std::vector<vertex_t> &leaves): leaves(leaves){}
+		template < typename Vertex, typename Graph >
+		void finish_vertex(Vertex u, const Graph & g) {
+			std::cout << "Exit " << u << std::endl; 
+			if (boost::out_degree(u, g)==0){
+				leaves.push_back(u);
+			}
+		}
+		std::vector<vertex_t> &leaves;
+	};
 
+	template <class Property>
+  	class label_writer {
+  	public:
+    	label_writer(Property property) : property(property) {}
+    	template <class VertexOrEdge>
+    	void operator()(std::ostream& out, const VertexOrEdge& v) const {
+    		out << "[label=\"" << property[v] << "\"]";
+    	}
+	private:
+		Property property;
+  };
 private:
 	graph_t graph_;
-
+	vertex_t genesis_; // root node
 
 };
 }
