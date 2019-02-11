1c86ba4891e71b138b5e1bfbff45afcb266a26f0
Chia-Chun Lin <starrynightglider@gmail.com>
2019-02-11 12:29:10 -0800

    add ConcurHash::clear()

--- concur_storage/concur_hash.hpp
+++ concur_storage/concur_hash.hpp
@@ -3,7 +3,7 @@
  * @Author: Chia-Chun Lin 
  * @Date: 2019-02-07 14:20:33 
  * @Last Modified by: Chia-Chun Lin
- * @Last Modified time: 2019-02-08 14:04:47
+ * @Last Modified time: 2019-02-11 11:25:37
  */
  
 #ifndef CONCUR_HASH_HPP
@@ -21,7 +21,7 @@ namespace taraxa{
 	// transaction type
 	using trx_t = std::string;
 
-	enum class ItemStatus: uint8_t {
+	enum class ConflictStatus: uint8_t {
 		read = 0, 
 		shared = 1, 
 		write = 2
@@ -34,22 +34,26 @@ namespace taraxa{
 		std::string storage;
 	};
 
-	class Item{
+	/** An Item should define
+	 * key, value
+	 * getHash()
+	 * operator ==
+	 */
+
+	class ConflictItem{
 	public:
-		Item (Addr const &addr, trx_t const &trx, ItemStatus status_);
+		ConflictItem (Addr const &addr, ConflictStatus status_);
 		std::size_t getHash() const { return std::hash<std::string> ()(addr_.contract+addr_.storage);}
-		bool operator<(Item const & other) const { return getHash() < other.getHash();}
-		bool operator==(Item const & other) const { return getHash() == other.getHash();}
-		friend std::ostream & operator<<(std::ostream &strm, Item const & item){
+		ConflictStatus & getStatus() {return status_;}
+		bool operator<(ConflictItem const & other) const { return getHash() < other.getHash();}
+		bool operator==(ConflictItem const & other) const { return getHash() == other.getHash();}
+		friend std::ostream & operator<<(std::ostream &strm, ConflictItem const & item){
 			strm<<"Addr: "<<item.addr_.contract<<" "<<item.addr_.storage<<" "<<asInteger(item.status_)<<"\n";
 			return strm;
 		}
 	private:
-		Addr addr_;
-		trx_t trx_;
-		ItemStatus status_;
-		bool dirty_;
-		trx_t old_val;
+		Addr addr_; //key
+		ConflictStatus status_; //value
 	};
 
 	/**
@@ -58,6 +62,7 @@ namespace taraxa{
 	 * number of locks = 2^(concur_degree)
 	 * 
 	 * Note: Do resize up but not down for now
+	 * Note: the hash content is expected to be modified through insertOrGet
 	 */
 	
 	template <typename Item> 
@@ -68,7 +73,8 @@ namespace taraxa{
 		ConcurHash (unsigned concur_degree_exp);
 		bool insert(Item item);
 		bool has(Item item);
-		bool remove(Item item) ;
+		bool remove(Item item);
+		void clear();
 
 		void setVerbose(bool verbose){ verbose_ = verbose;}
 		std::size_t getConcurrentDegree();
@@ -77,20 +83,20 @@ namespace taraxa{
 		uint64_t getItemSize(); 
 	
 	private:
-
 		unsigned getBucketId(Item item) const;
 		unsigned getLockId(Item item) const;
 		void resize(); // double buckets_ size
 		bool policy() const; // determines when to do resize
 		bool verbose_;
 		const std::size_t concur_degree_; 
+		const std::size_t init_bucket_size_;
 		std::atomic<uint64_t> total_size_;  // total number of items
 		std::atomic<std::size_t> bucket_size_;
 		std::vector<bucket> buckets_;
 		std::vector<std::mutex> mutexes_; 
 	};
 	
-	using ConflictHash = ConcurHash<Item>;
+	using ConflictHash = ConcurHash<ConflictItem>;
 
 	}// namespace storage
 }// namespace taraxa
