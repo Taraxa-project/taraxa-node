8fdccd5e414450e781aafabf6868484d8a130d95
Mm <miho.frankovic@gmail.com>
2019-03-04 16:55:41 +0100

    Added block p2p test

--- core_tests/p2p_test.cpp
+++ core_tests/p2p_test.cpp
@@ -53,8 +53,8 @@ TEST(p2p, capability)
 {
     int const step = 10;
     const char* const localhost = "127.0.0.1";
-    NetworkConfig prefs1(localhost, 0, false /* upnp */, true /* allow local discovery */);
-    NetworkConfig prefs2(localhost, 0, false /* upnp */, true /* allow local discovery */);
+    NetworkConfig prefs1(localhost, 0, false , true );
+    NetworkConfig prefs2(localhost, 0, false , true );
     Host host1("Test", prefs1);
     Host host2("Test", prefs2);
     auto thc1 = make_shared<TaraxaCapability>(host1);
@@ -103,6 +103,73 @@ TEST(p2p, capability)
     std::pair<int, int> testData = thc1->retrieveTestData(host2.id());
     EXPECT_EQ(target, testData.first);
     EXPECT_EQ(checksum, testData.second);
+}
+
+
+TEST(p2p, block)
+{
+    int const step = 10;
+    const char* const localhost = "127.0.0.1";
+    NetworkConfig prefs1(localhost, 0, false /* upnp */, true /* allow local discovery */);
+    NetworkConfig prefs2(localhost, 0, false /* upnp */, true /* allow local discovery */);
+    Host host1("Test", prefs1);
+    Host host2("Test", prefs2);
+    auto thc1 = make_shared<TaraxaCapability>(host1);
+    host1.registerCapability(thc1);
+    auto thc2 = make_shared<TaraxaCapability>(host2);
+    host2.registerCapability(thc2);
+    host1.start();
+    host2.start();
+    auto port1 = host1.listenPort();
+    auto port2 = host2.listenPort();
+    EXPECT_NE(port1, 0);
+    EXPECT_NE(port2, 0);
+    EXPECT_NE(port1, port2);
+
+    for (unsigned i = 0; i < 3000; i += step)
+    {
+        this_thread::sleep_for(chrono::milliseconds(step));
+
+        if (host1.isStarted() && host2.isStarted())
+            break;
+    }
+
+    EXPECT_TRUE(host1.isStarted());
+    EXPECT_TRUE(host2.isStarted());
+    host1.requirePeer(
+        host2.id(), NodeIPEndpoint(bi::address::from_string(localhost), port2, port2));
+
+    // Wait for up to 12 seconds, to give the hosts time to connect to each other.
+    for (unsigned i = 0; i < 12000; i += step)
+    {
+        this_thread::sleep_for(chrono::milliseconds(step));
+
+        if ((host1.peerCount() > 0) && (host2.peerCount() > 0))
+            break;
+    }
+
+    EXPECT_GT(host1.peerCount(), 0);
+    EXPECT_GT(host2.peerCount(), 0);
+
+    StateBlock blk (
+	"1111111111111111111111111111111111111111111111111111111111111111",
+	{
+	"2222222222222222222222222222222222222222222222222222222222222222",
+	"3333333333333333333333333333333333333333333333333333333333333333",
+	"4444444444444444444444444444444444444444444444444444444444444444"}, 
+	{
+	"5555555555555555555555555555555555555555555555555555555555555555",
+	"6666666666666666666666666666666666666666666666666666666666666666"},
+	"77777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777",
+	"8888888888888888888888888888888888888888888888888888888888888888",
+	"000000000000000000000000000000000000000000000000000000000000000F");
+
+    thc2->sendBlock(host1.id(), blk);
+
+    this_thread::sleep_for(chrono::seconds(1));
+    auto blocks = thc1->getBlocks();
+    EXPECT_EQ(blocks.size(), 1);
+    EXPECT_EQ(blk, blocks[0]);
 }
 }  // namespace taraxa
 
