337256bbdbeed3ecbda7ff31462dcd265e5104ae
Mm <miho.frankovic@gmail.com>
2019-03-13 16:43:59 +0100

    block propagation

--- taraxa_capability.h
+++ taraxa_capability.h
@@ -5,24 +5,45 @@
 #include <libp2p/Session.h>
 #include <chrono>
 #include <thread>
+#include <set>
 #include "state_block.hpp"
 
 using namespace std;
 using namespace dev;
 using namespace dev::p2p;
-using namespace taraxa;
+
+namespace taraxa {
 
 enum SubprotocolPacketType: ::byte
 {
-    BlockPacket = 0x0,
+    NewBlockPacket = 0x0,
+    NewBlockHashPacket,
+    GetBlockPacket,
     TestPacket,
     PacketCount
+};
+
+class TaraxaPeer {
+public:
+    TaraxaPeer(){}
+    TaraxaPeer(NodeID id):m_id(id){}
+
+    bool isBlockKnown(blk_hash_t const& _hash) const { return m_knownBlocks.count(_hash); }
+    void markBlockAsKnown(blk_hash_t const& _hash) { m_knownBlocks.insert(_hash); }
+    void clearKnownBlocks() { m_knownBlocks.clear(); }
+
+private:
+    std::set<blk_hash_t> m_knownBlocks;
+    NodeID m_id;
 };
 
 class TaraxaCapability : public CapabilityFace, public Worker
 {
 public:
-    TaraxaCapability(Host const& _host) : Worker("taraxa"), m_host(_host) {}
+    TaraxaCapability(Host const& _host) : Worker("taraxa"), m_host(_host) {
+        std::random_device seed;
+        m_urng = std::mt19937_64(seed());
+    }
 
     std::string name() const override { return "taraxa"; }
     unsigned version() const override { return 1; }
@@ -35,11 +56,15 @@ public:
     {
         m_cntReceivedMessages[_nodeID] = 0;
         m_testSums[_nodeID] = 0;
+
+        TaraxaPeer peer(_nodeID);
+        m_peers.emplace(_nodeID, peer);
     }
     bool interpretCapabilityPacket(NodeID const& _nodeID, unsigned _id, RLP const& _r) override
     {
         switch(_id) {
-            case BlockPacket: {
+            case NewBlockPacket: {
+                //printf("Received NewBlockPacket\n");
                 std::vector<::byte> blockBytes;
                 for(auto i = 0; i < _r[0].itemCount(); i++) {
                     blockBytes.push_back(_r[0][i].toInt());
@@ -47,15 +72,49 @@ public:
                 taraxa::bufferstream strm(blockBytes.data(), blockBytes.size());
 	            StateBlock block;
 	            block.deserialize(strm);
-                m_receivedBlocks.push_back(block);
+
+                m_peers[_nodeID].markBlockAsKnown(block.getHash());
+                if(m_blocks.find(block.getHash()) == m_blocks.end()) {
+                  onNewBlock(block);
+                  printf("Received NewBlock\n");
+                }
+                break;
+            }
+            case NewBlockHashPacket: {
+                //printf("Received NewBlockHashPacket\n");
+                std::vector<::byte> blockBytes;
+                for(auto i = 0; i < _r[0].itemCount(); i++) {
+                    blockBytes.push_back(_r[0][i].toInt());
+                }
+                blk_hash_t hash;
+                memcpy(&hash, blockBytes.data(), blockBytes.size());
+                if(m_blocks.find(hash) == m_blocks.end() && blockRequestedSet.count(hash) == 0) {
+                    blockRequestedSet.insert(hash);
+                    requestBlock(_nodeID, hash);
+                }
+	            break;
+            }
+            case GetBlockPacket: {
+                //printf("Received GetBlockPacket\n");
+                std::vector<::byte> blockBytes;
+                for(auto i = 0; i < _r[0].itemCount(); i++) {
+                    blockBytes.push_back(_r[0][i].toInt());
+                }
+                blk_hash_t hash;
+                memcpy(&hash, blockBytes.data(), blockBytes.size());
+                if(m_blocks.find(hash) != m_blocks.end()) {
+                    sendBlock(_nodeID, m_blocks[hash]);
+                }
                 break;
             }
             case TestPacket:
+                printf("Received TestPacket\n");
                 ++m_cntReceivedMessages[_nodeID];
                 m_testSums[_nodeID] += _r[0].toInt();
                 BOOST_ASSERT(_id == TestPacket);
                 return (_id == TestPacket);
         }; 
+        return true;
     }
     void onDisconnect(NodeID const& _nodeID) override
     {
@@ -70,7 +129,78 @@ public:
             _id, m_host.capabilityHost()->prep(_id, name(), s, TestPacket, 1) << _x);
     }
 
+    vector<NodeID> selectPeers(
+    std::function<bool(TaraxaPeer const&)> const& _predicate) const
+    {
+        vector<NodeID> allowed;
+        for (auto const& peer : m_peers)
+        {
+            if (_predicate(peer.second))
+                allowed.push_back(peer.first);
+        }
+        return allowed;
+    }
+
+    std::pair<std::vector<NodeID>, std::vector<NodeID>> randomPartitionPeers(
+    std::vector<NodeID> const& _peers, std::size_t _number) const
+    {
+        vector<NodeID> part1(_peers);
+        vector<NodeID> part2;
+
+        if (_number >= _peers.size())
+            return std::make_pair(part1, part2);
+
+        std::shuffle(part1.begin(), part1.end(), m_urng);
+
+        // Remove elements from the end of the shuffled part1 vector and move them to part2.
+        std::move(part1.begin() + _number, part1.end(), std::back_inserter(part2));
+        part1.erase(part1.begin() + _number, part1.end());
+        return std::make_pair(move(part1), move(part2));
+    }
+
+    void onNewBlock(StateBlock block) {
+        const int c_minBlockBroadcastPeers = 1;
+        m_blocks[block.getHash()] = block;
+        auto const peersWithoutBlock = selectPeers(
+            [&](TaraxaPeer const& _peer) { return !_peer.isBlockKnown(block.getHash()); });
+
+        auto const peersToSendNumber =
+            std::max<std::size_t>(c_minBlockBroadcastPeers, std::sqrt(m_peers.size()));
+
+        std::vector<NodeID> peersToSend;
+        std::vector<NodeID> peersToAnnounce;
+        std::tie(peersToSend, peersToAnnounce) =
+            randomPartitionPeers(peersWithoutBlock, peersToSendNumber);
+
+
+        for (NodeID const& peerID : peersToSend) {
+            RLPStream ts;
+            auto itPeer = m_peers.find(peerID);
+            if (itPeer != m_peers.end())
+            {
+                sendBlock(peerID, block);
+                itPeer->second.markBlockAsKnown(block.getHash());
+            }
+        }
+        if (!peersToSend.empty())
+            printf("Sent block to %lu peers\n", peersToSend.size());
+
+        for (NodeID const& peerID : peersToAnnounce)
+        {
+            RLPStream ts;
+            auto itPeer = m_peers.find(peerID);
+            if (itPeer != m_peers.end())
+            {
+                sendBlockHash(peerID, block);
+                itPeer->second.markBlockAsKnown(block.getHash());
+            }
+        }
+        if (!peersToAnnounce.empty())
+            printf("Anounced block to %lu peers\n", peersToAnnounce.size());
+    }
+
     void sendBlock(NodeID const& _id, taraxa::StateBlock block) {
+        //printf("sendBlock \n");
         RLPStream s;
         std::vector<uint8_t> bytes;
         // Need to put a scope of vectorstream, other bytes won't get result.
@@ -78,10 +208,35 @@ public:
             vectorstream strm(bytes);
             block.serialize(strm);
         }
-        m_host.capabilityHost()->prep(_id, name(), s, BlockPacket, 1);
+        m_host.capabilityHost()->prep(_id, name(), s, NewBlockPacket, 1);
         s.appendList(bytes.size());
         for(auto i = 0; i < bytes.size(); i++)
             s << bytes[i];
+        m_host.capabilityHost()->sealAndSend(
+            _id, s);
+    }
+
+    void sendBlockHash(NodeID const& _id, taraxa::StateBlock block) {
+        //printf("sendBlockHash \n");
+        RLPStream s;
+        std::vector<uint8_t> bytes;
+        m_host.capabilityHost()->prep(_id, name(), s, NewBlockHashPacket, 1);
+        auto hash = block.getHash();
+        s.appendList(sizeof(hash));
+        for(auto i = 0; i < sizeof(hash); i++)
+            s << ((char *)&hash)[i];
+        m_host.capabilityHost()->sealAndSend(
+            _id, s);
+    }
+
+    void requestBlock(NodeID const& _id, blk_hash_t hash) {
+        //printf("requestBlock \n");
+        RLPStream s;
+        std::vector<uint8_t> bytes;
+        m_host.capabilityHost()->prep(_id, name(), s, GetBlockPacket, 1);
+        s.appendList(sizeof(hash));
+        for(auto i = 0; i < sizeof(hash); i++)
+            s << ((char *)&hash)[i];
         m_host.capabilityHost()->sealAndSend(
             _id, s);
     }
@@ -100,12 +255,17 @@ public:
         return {cnt, checksum};
     }
 
-    std::vector<taraxa::StateBlock> getBlocks() {
-        return m_receivedBlocks;
+    std::map<blk_hash_t, taraxa::StateBlock> getBlocks() {
+        return m_blocks;
     }
 
     Host const& m_host;
     std::unordered_map<NodeID, int> m_cntReceivedMessages;
     std::unordered_map<NodeID, int> m_testSums;
-    std::vector<taraxa::StateBlock> m_receivedBlocks;
+    std::map<blk_hash_t, taraxa::StateBlock> m_blocks;
+    std::set<blk_hash_t> blockRequestedSet;
+
+    std::unordered_map<NodeID, TaraxaPeer> m_peers;
+    mutable std::mt19937_64 m_urng;  // Mersenne Twister psuedo-random number generator
 };
+}
