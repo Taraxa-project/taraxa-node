80963e260f924cb7319814a5feff9c3d13504d1b
icsjerry <icsjerry@gmail.com>
2019-03-20 11:14:42 -0700

    fix for gtest, renames, comments on db test files

--- core_tests/overlaydb_test.cpp
+++ core_tests/overlaydb_test.cpp
@@ -1,29 +1,18 @@
 /*
-    This file is part of cpp-ethereum.
-
-    cpp-ethereum is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    cpp-ethereum is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    This file is a modified version of cpp-ethereum unit-test for OverlayDB.
 */
 
 #include <libdevcore/DBFactory.h>
 #include <libdevcore/OverlayDB.h>
 
 #include <gtest/gtest.h>
+#include "libdevcore/Log.h"
 
 using namespace std;
 using namespace dev;
 using namespace db;
 
+namespace taraxa{
 TEST(OverlayDB, basicUsage)
 {
     std::unique_ptr<db::DatabaseFace> db = DBFactory::create(DatabaseKind::MemoryDB);
@@ -103,4 +92,14 @@ TEST(OverlayDB, rollback)
     EXPECT_TRUE(odb.get().size());
     odb.rollback();
     EXPECT_TRUE(!odb.get().size());
+}
+
+}  //namespace taraxa
+
+int main(int argc, char** argv){
+    dev::LoggingOptions logOptions;
+    logOptions.verbosity = dev::VerbositySilent;
+    dev::setupLogging(logOptions);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
