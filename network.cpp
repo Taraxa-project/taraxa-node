c70bfae74cc8b3d2dbcfe2df8b0fb2a22f180adb
gaoqi8649@gmail.com <gaoqi8649@gmail.com>
2019-04-19 10:38:07 -0700

    fix typo issue

--- network.cpp
+++ network.cpp
@@ -135,7 +135,7 @@ void Network::saveNetwork(std::string fileName) {
   if (!netData.empty()) writeFile(fileName, &netData);
 }
 
-void Network::noNewPbftVote(Vote const &vote) {
+void Network::onNewPbftVote(Vote const &vote) {
   LOG(logger_debug_) << "Network broadcast PBFT vote: "
                      << vote.getHash().toString();
   taraxa_capability_->onNewPbftVote(vote);
