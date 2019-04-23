955dd9df3c3ddf99cdb4af5ae7a0a4720f6d1c9f
Chia-Chun Lin <starrynightglider@gmail.com>
2019-04-22 23:35:44 -0700

    fix proposer bug

--- block_proposer.cpp
+++ block_proposer.cpp
@@ -13,12 +13,17 @@
 namespace taraxa {
 
 std::atomic<uint64_t> BlockProposer::num_proposed_blocks = 0;
+std::mt19937 RandomPropose::generator;
 
 bool RandomPropose::propose() {
-  auto delay = distribution_(gen_);
+  auto delay = distribution_(RandomPropose::generator);
   thisThreadSleepForMilliSeconds(delay);
-  LOG(log_nf_) << "Add proposer delay " << delay << std::endl;
-
+  LOG(log_tr_) << "Add proposer delay " << delay << std::endl;
+  if (proposer_.expired()) {
+    LOG(log_er_) << "Block proposer not available" << std::endl;
+    return false;
+  }
+  proposer_.lock()->proposeBlock();
   return true;
 }
 
@@ -47,61 +52,62 @@ void BlockProposer::start() {
   // reset number of proposed blocks
   BlockProposer::num_proposed_blocks = 0;
   propose_model_->setProposer(getShared());
+  proposer_worker_ = std::make_shared<std::thread>([this]() {
+    while (!stopped_) {
+      propose_model_->propose();
+    }
+  });
 }
-
 void BlockProposer::stop() {
+  if (stopped_) return;
   stopped_ = true;
-  if (!trx_mgr_.expired()) {
-    trx_mgr_.lock()->stop();
-  }
+  proposer_worker_->join();
 }
 void BlockProposer::proposeBlock() {
-  while (!stopped_) {
-    std::string pivot;
-    std::vector<std::string> tips;
-    vec_trx_t to_be_packed_trx;
-    if (trx_mgr_.expired()) {
-      LOG(log_wr_) << "TransactionManager expired ..." << std::endl;
-      break;
-    }
+  std::string pivot;
+  std::vector<std::string> tips;
+  vec_trx_t to_be_packed_trx;
+  if (trx_mgr_.expired()) {
+    LOG(log_wr_) << "TransactionManager expired ..." << std::endl;
+    return;
+  }
 
-    trx_mgr_.lock()->packTrxs(to_be_packed_trx);
-    if (to_be_packed_trx.empty()) {
-      LOG(log_wr_) << "Skip block proposer, zero unpacked transactions ..."
-                   << std::endl;
-      continue;
+  trx_mgr_.lock()->packTrxs(to_be_packed_trx);
+  if (to_be_packed_trx.empty()) {
+    LOG(log_tr_) << "Skip block proposer, zero unpacked transactions ..."
+                 << std::endl;
+    return;
+  }
+  if (dag_mgr_.expired()) {
+    LOG(log_wr_) << "DagManager expired ..." << std::endl;
+    return;
+  }
+  bool ok = dag_mgr_.lock()->getLatestPivotAndTips(pivot, tips);
+  if (ok) {
+    LOG(log_nf_) << "BlockProposer: pivot: " << pivot
+                 << ", tip size = " << tips.size() << std::endl;
+    LOG(log_nf_) << "Tips: " << std::endl;
+    for (auto const& s : tips) {
+      LOG(log_nf_) << s << std::endl;
     }
-    if (dag_mgr_.expired()) {
-      LOG(log_wr_) << "DagManager expired ..." << std::endl;
-      break;
-    }
-    bool ok = dag_mgr_.lock()->getLatestPivotAndTips(pivot, tips);
-    if (ok) {
-      LOG(log_nf_) << "BlockProposer: pivot: " << pivot
-                   << ", tip size = " << tips.size() << std::endl;
-      LOG(log_nf_) << "Tips: " << std::endl;
-      for (auto const& s : tips) {
-        LOG(log_nf_) << s << std::endl;
-      }
-      LOG(log_nf_) << std::endl;
-      BlockProposer::num_proposed_blocks.fetch_add(1);
-    } else {
-      LOG(log_wr_) << "Pivot and tips unavailable ..." << std::endl;
-    }
-    vec_blk_t tmp;
-    for (auto const& t : tips) {
-      tmp.emplace_back(blk_hash_t(t));
-    }
-    DagBlock blk(blk_hash_t(pivot), tmp, to_be_packed_trx);
+    LOG(log_nf_) << std::endl;
+    BlockProposer::num_proposed_blocks.fetch_add(1);
+  } else {
+    LOG(log_wr_) << "Pivot and tips unavailable ..." << std::endl;
+  }
+  vec_blk_t tmp;
+  for (auto const& t : tips) {
+    tmp.emplace_back(blk_hash_t(t));
+  }
+  DagBlock blk(blk_hash_t(pivot), tmp, to_be_packed_trx);
 
-    if (!full_node_.expired()) {
-      LOG(log_nf_) << "Propose block" << std::endl;
+  if (!full_node_.expired()) {
+    LOG(log_nf_) << "Propose block" << std::endl;
 
-      full_node_.lock()->storeBlockAndSign(blk);
-    } else {
-      LOG(log_er_) << "FullNode unavailable ..." << std::endl;
-      return;
-    }
+    full_node_.lock()->storeBlockAndSign(blk);
+  } else {
+    LOG(log_er_) << "FullNode unavailable ..." << std::endl;
+    return;
   }
 }
 
