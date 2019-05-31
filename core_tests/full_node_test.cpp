6572e790a20d0aa88bc80927d909db0242cccd5e
Chia-Chun Lin <starrynightglider@gmail.com>
2019-05-31 13:56:45 -0700

    add test: Top.sync_five_node_simple

--- core_tests/full_node_test.cpp
+++ core_tests/full_node_test.cpp
@@ -33,6 +33,263 @@ auto g_trx_signed_samples =
     samples::createSignedTrxSamples(0, NUM_TRX, g_secret);
 auto g_mock_dag0 = samples::createMockDag0();
 
+TEST(Top, sync_five_nodes_simple) {
+  const char* input1[] = {"./build/main", "--conf_taraxa",
+                          "./core_tests/conf_taraxa1.json", "-v", "0"};
+  const char* input2[] = {"./build/main2", "--conf_taraxa",
+                          "./core_tests/conf_taraxa2.json", "-v", "0"};
+  const char* input3[] = {"./build/main3", "--conf_taraxa",
+                          "./core_tests/conf_taraxa3.json", "-v", "0"};
+  const char* input4[] = {"./build/main4", "--conf_taraxa",
+                          "./core_tests/conf_taraxa4.json", "-v", "0"};
+  const char* input5[] = {"./build/main4", "--conf_taraxa",
+                          "./core_tests/conf_taraxa5.json", "-v", "0"};
+
+  // copy main2, main3, main4, main5
+  try {
+    std::cout << "Copying main2 ..." << std::endl;
+    system("cp ./build/main ./build/main2");
+    std::cout << "Copying main3 ..." << std::endl;
+    system("cp ./build/main ./build/main3");
+    std::cout << "Copying main4 ..." << std::endl;
+    system("cp ./build/main ./build/main4");
+    std::cout << "Copying main5 ..." << std::endl;
+    system("cp ./build/main ./build/main5");
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  Top top1(5, input1);
+  EXPECT_TRUE(top1.isActive());
+  taraxa::thisThreadSleepForMilliSeconds(1000);
+  std::cout << "Top1 created ..." << std::endl;
+
+  Top top2(5, input2);
+  EXPECT_TRUE(top2.isActive());
+  std::cout << "Top2 created ..." << std::endl;
+
+  Top top3(5, input3);
+  EXPECT_TRUE(top3.isActive());
+  std::cout << "Top3 created ..." << std::endl;
+
+  Top top4(5, input4);
+  EXPECT_TRUE(top4.isActive());
+  std::cout << "Top4 created ..." << std::endl;
+
+  Top top5(5, input5);
+  EXPECT_TRUE(top5.isActive());
+  std::cout << "Top5 created ..." << std::endl;
+  // wait for top2, top3, top4, top5 initialize
+  taraxa::thisThreadSleepForMilliSeconds(2000);
+
+  // send 1000 trxs
+  try {
+    std::string sendtrx1 =
+        R"(curl -s -d '{"action": "create_test_coin_transactions", 
+                                      "secret": "3800b2875669d9b2053c1aff9224ecfdc411423aac5b5a73d7a45ced1c3b9dce", 
+                                      "delay": 5, 
+                                      "number": 6000, 
+                                      "seed": 1 }' 0.0.0.0:7777)";
+    std::string sendtrx2 =
+        R"(curl -s -d '{"action": "create_test_coin_transactions", 
+                                      "secret": "3800b2875669d9b2053c1aff9224ecfdc411423aac5b5a73d7a45ced1c3b9dcd", 
+                                      "delay": 7, 
+                                      "number": 4000, 
+                                      "seed": 2 }' 0.0.0.0:7778)";
+    std::string sendtrx3 =
+        R"(curl -s -d '{"action": "create_test_coin_transactions", 
+                                      "secret": "3800b2875669d9b2053c1aff9224ecfdc411423aac5b5a73d7a45ced1c3b9dc1", 
+                                      "delay": 3, 
+                                      "number": 3000, 
+                                      "seed": 3 }' 0.0.0.0:7779)";
+    std::string sendtrx4 =
+        R"(curl -s -d '{"action": "create_test_coin_transactions", 
+                                      "secret": "3800b2875669d9b2053c1aff9224ecfdc411423aac5b5a73d7a45ced1c3b9dc2", 
+                                      "delay": 10, 
+                                      "number": 3000, 
+                                      "seed": 4 }' 0.0.0.0:7780)";
+    std::string sendtrx5 =
+        R"(curl -s -d '{"action": "create_test_coin_transactions", 
+                                      "secret": "3800b2875669d9b2053c1aff9224ecfdc411423aac5b5a73d7a45ced1c3b9dc3", 
+                                      "delay": 23, 
+                                      "number": 4000, 
+                                      "seed": 5 }' 0.0.0.0:7781)";
+    std::cout << "Sending trxs ..." << std::endl;
+    std::thread t1([sendtrx1]() { system(sendtrx1.c_str()); });
+    std::thread t2([sendtrx2]() { system(sendtrx2.c_str()); });
+    std::thread t3([sendtrx3]() { system(sendtrx3.c_str()); });
+    std::thread t4([sendtrx4]() { system(sendtrx4.c_str()); });
+    std::thread t5([sendtrx5]() { system(sendtrx5.c_str()); });
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    std::cout << "All trxs sent..." << std::endl;
+
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  auto node1 = top1.getNode();
+  auto node2 = top2.getNode();
+  auto node3 = top3.getNode();
+  auto node4 = top4.getNode();
+  auto node5 = top5.getNode();
+
+  EXPECT_NE(node1, nullptr);
+  EXPECT_NE(node2, nullptr);
+  EXPECT_NE(node3, nullptr);
+  EXPECT_NE(node4, nullptr);
+  EXPECT_NE(node5, nullptr);
+
+  // // check transactions
+  // auto knowntrx1 = node1->getNumKnownTransactions();
+  // auto knowntrx2 = node2->getNumKnownTransactions();
+  // auto knowntrx3 = node3->getNumKnownTransactions();
+  // auto knowntrx4 = node3->getNumKnownTransactions();
+  // auto knowntrx5 = node3->getNumKnownTransactions();
+
+  // for (auto i = 0; i < 180; i++) {
+  //   if (i % 10 == 0) {
+  //     std::cout << "Wait for trxs syncing ..." << std::endl;
+  //   }
+  //   knowntrx1 = node1->getNumKnownTransactions();
+  //   knowntrx2 = node2->getNumKnownTransactions();
+  //   knowntrx3 = node3->getNumKnownTransactions();
+  //   knowntrx4 = node4->getNumKnownTransactions();
+  //   knowntrx5 = node5->getNumKnownTransactions();
+
+  //   if (knowntrx1 == knowntrx2 && knowntrx2 == knowntrx3 &&
+  //       knowntrx3 == knowntrx4 && knowntrx4 == knowntrx5 && knowntrx5 ==
+  //       20000)
+  //     break;
+  //   taraxa::thisThreadSleepForMilliSeconds(500);
+  // }
+
+  // // check blocks
+  // auto knownBlocks1 = node1->getNumKnownBlocks();
+  // auto knownBlocks2 = node2->getNumKnownBlocks();
+  // auto knownBlocks3 = node3->getNumKnownBlocks();
+  // auto knownBlocks4 = node4->getNumKnownBlocks();
+  // auto knownBlocks5 = node5->getNumKnownBlocks();
+
+  // for (auto i = 0; i < 180; i++) {
+  //   if (i % 10 == 0) {
+  //     std::cout << "Wait for blocks syncing ..." << std::endl;
+  //   }
+  //   knownBlocks1 = node1->getNumKnownBlocks();
+  //   knownBlocks2 = node2->getNumKnownBlocks();
+  //   knownBlocks3 = node3->getNumKnownBlocks();
+  //   knownBlocks4 = node4->getNumKnownBlocks();
+  //   knownBlocks5 = node5->getNumKnownBlocks();
+
+  //   if (knownBlocks1 == knownBlocks2 && knownBlocks2 == knownBlocks3 &&
+  //       knownBlocks3 == knownBlocks4 && knownBlocks4 == knownBlocks5 &&
+  //       (knownBlocks5 > 0))
+  //     break;
+  //   taraxa::thisThreadSleepForMilliSeconds(500);
+  // }
+
+  // check dags
+  auto numVertices1 = node1->getNumVerticesInDag();
+  auto numVertices2 = node2->getNumVerticesInDag();
+  auto numVertices3 = node3->getNumVerticesInDag();
+  auto numVertices4 = node4->getNumVerticesInDag();
+  auto numVertices5 = node5->getNumVerticesInDag();
+
+  for (auto i = 0; i < 180; i++) {
+    if (i % 10 == 0) {
+      std::cout << "Wait for vertices syncing ..." << std::endl;
+    }
+    numVertices1 = node1->getNumVerticesInDag();
+    numVertices2 = node2->getNumVerticesInDag();
+    numVertices3 = node3->getNumVerticesInDag();
+    numVertices4 = node4->getNumVerticesInDag();
+    numVertices5 = node5->getNumVerticesInDag();
+
+    if (numVertices1 == numVertices2 && numVertices2 == numVertices3 &&
+        numVertices3 == numVertices4 && numVertices4 == numVertices5)
+      break;
+    taraxa::thisThreadSleepForMilliSeconds(500);
+  }
+
+  // knowntrx1 = node1->getNumKnownTransactions();
+  // knowntrx2 = node2->getNumKnownTransactions();
+  // knowntrx3 = node3->getNumKnownTransactions();
+  // knowntrx4 = node4->getNumKnownTransactions();
+  // knowntrx5 = node5->getNumKnownTransactions();
+
+  // EXPECT_EQ(knowntrx1, knowntrx2);
+  // EXPECT_EQ(knowntrx2, knowntrx3);
+  // EXPECT_EQ(knowntrx3, knowntrx4);
+  // EXPECT_EQ(knowntrx4, knowntrx5);
+  // EXPECT_EQ(knowntrx5, 20000);
+
+  // knownBlocks1 = node1->getNumKnownBlocks();
+  // knownBlocks2 = node2->getNumKnownBlocks();
+  // knownBlocks3 = node3->getNumKnownBlocks();
+  // knownBlocks4 = node4->getNumKnownBlocks();
+  // knownBlocks5 = node5->getNumKnownBlocks();
+
+  // EXPECT_EQ(knownBlocks1, knownBlocks2);
+  // EXPECT_EQ(knownBlocks2, knownBlocks3);
+  // EXPECT_EQ(knownBlocks3, knownBlocks4);
+  // EXPECT_EQ(knownBlocks4, knownBlocks5);
+
+  // auto verifiedBlocks1 = node1->getNumVerifiedBlocks();
+  // auto verifiedBlocks2 = node2->getNumVerifiedBlocks();
+  // auto verifiedBlocks3 = node3->getNumVerifiedBlocks();
+  // auto verifiedBlocks4 = node4->getNumVerifiedBlocks();
+  // auto verifiedBlocks5 = node5->getNumVerifiedBlocks();
+  // EXPECT_EQ(knownBlocks1, verifiedBlocks1)
+  //     << " Unverified Transactions: "
+  //     << ::testing::PrintToString(node1->getNumUnverifiedTransactions());
+  // EXPECT_EQ(knownBlocks2, verifiedBlocks2)
+  //     << " Unverified Transactions: "
+  //     << ::testing::PrintToString(node2->getNumUnverifiedTransactions());
+  // EXPECT_EQ(knownBlocks3, verifiedBlocks3)
+  //     << " Unverified Transactions: "
+  //     << ::testing::PrintToString(node3->getNumUnverifiedTransactions());
+  // EXPECT_EQ(knownBlocks4, verifiedBlocks4)
+  //     << " Unverified Transactions: "
+  //     << ::testing::PrintToString(node4->getNumUnverifiedTransactions());
+  // EXPECT_EQ(knownBlocks5, verifiedBlocks5)
+  //     << " Unverified Transactions: "
+  //     << ::testing::PrintToString(node5->getNumUnverifiedTransactions());
+
+  numVertices1 = node1->getNumVerticesInDag();
+  numVertices2 = node2->getNumVerticesInDag();
+  numVertices3 = node3->getNumVerticesInDag();
+  numVertices4 = node4->getNumVerticesInDag();
+  numVertices5 = node5->getNumVerticesInDag();
+
+  EXPECT_EQ(numVertices1, numVertices2);
+  EXPECT_EQ(numVertices2, numVertices3);
+  EXPECT_EQ(numVertices3, numVertices4);
+  EXPECT_EQ(numVertices4, numVertices5);
+
+  top5.stop();
+  top4.stop();
+  top3.stop();
+  top2.stop();
+  top1.stop();
+  // delete main2
+  try {
+    std::cout << "main5 deleted ..." << std::endl;
+    system("rm -f ./build/main5");
+    std::cout << "main4 deleted ..." << std::endl;
+    system("rm -f ./build/main4");
+    std::cout << "main3 deleted ..." << std::endl;
+    system("rm -f ./build/main3");
+    std::cout << "main2 deleted ..." << std::endl;
+    system("rm -f ./build/main2");
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
 TEST(FullNode, insert_anchor_and_compute_order) {
   boost::asio::io_context context;
   auto node(std::make_shared<taraxa::FullNode>(
