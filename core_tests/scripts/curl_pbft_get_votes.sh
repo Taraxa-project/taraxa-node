9407321f2ea90e83aeba787847f0c8887736aafa
Mm <miho.frankovic@gmail.com>
2019-06-20 16:44:00 +0200

    rpc temp commit

--- core_tests/scripts/curl_pbft_get_votes.sh
+++ core_tests/scripts/curl_pbft_get_votes.sh
@@ -2,8 +2,10 @@
 
 set -B
   
-cmd="{ \"action\": \"get_votes\",
-       \"period\": 2
+cmd="{ \"jsonrpc\": \"2.0\",
+		 \"id\": \"2\",
+         \"method\": \"get_votes\", 
+         \"params\": [{\"period\": 2}]
     }"
 
 curl -d "$cmd" 0.0.0.0:7777 &>/dev/null
--- core_tests/scripts/curl_pbft_should_speak.sh
+++ core_tests/scripts/curl_pbft_should_speak.sh
@@ -2,10 +2,12 @@
 
 set -B
   
-cmd="{ \"action\": \"should_speak\",
-       \"type\": \"1\",
+cmd="{ \"jsonrpc\": \"2.0\",
+	   \"id\": \"4\",
+       \"method\": \"should_speak\",
+       \"params\": [\"type\": \"1\",
        \"period\": 1234,
        \"step\": 56
-    }"
+    ]}}"
 
 curl -d "$cmd" 0.0.0.0:7777 &>/dev/null
