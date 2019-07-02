59bdc5ed2b9f9682eeebd545ed014670407c9cac
Alex Moore <alex.moore@toptal.com>
2019-07-01 22:44:36 -0600

    Change stage Build Docker Image to use try catch

--- base-image-build.pipeline.groovy
+++ base-image-build.pipeline.groovy
@@ -15,15 +15,13 @@ pipeline {
             }
         }
         stage('Build Docker Image') {
-            steps {
-                try {
-                    sh 'docker pull ${REGISTRY}/${IMAGE}'
-                }
-                catch (exc) {
-                    echo 'Something failed, I should sound the klaxons!'
-                }
-                sh 'docker build --pull --cache-from=${REGISTRY}/${IMAGE} -t ${IMAGE}-${BRANCH_NAME_LOWER_CASE}-${BUILD_NUMBER} -f dockerfiles/base.ubuntu.dockerfile .'
+            try {
+                sh 'docker pull ${REGISTRY}/${IMAGE}'
             }
+            catch (exc) {
+                echo 'Something failed, I should sound the klaxons!'
+            }
+            sh 'docker build --pull --cache-from=${REGISTRY}/${IMAGE} -t ${IMAGE}-${BRANCH_NAME_LOWER_CASE}-${BUILD_NUMBER} -f dockerfiles/base.ubuntu.dockerfile .'
         }
         stage('Push Docker Image') {
             when { branch 'master' }
