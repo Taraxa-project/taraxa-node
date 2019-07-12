5d41576e229af1dcf6d2eda82ede3efb68f4f92c
Oleh L <02p01r@gmail.com>
2019-07-12 17:31:11 +0300

    Added a common c++ extension to formatting search path

--- scripts/format_project_files_cxx.sh
+++ scripts/format_project_files_cxx.sh
@@ -6,7 +6,8 @@ cd ..
 
 find . concur_storage core_tests \
   -maxdepth 1 \
-  -iname '*.h' -o -iname '*.c' -o -iname '*.cpp' -o -iname '*.hpp' |
+  -iname '*.h' -o -iname '*.c' -o -iname '*.cpp' -o -iname '*.hpp' \
+  -o -iname '*.hxx' -o -iname '*.cxx' |
   xargs clang-format -style=file -i -fallback-style=none
 
 cd ${PREV_CWD}
