b45c448b4518fe2e490ee7ad0fe52ae58a171a52
Oleh L <02p01r@gmail.com>
2019-07-12 17:26:52 +0300

    Reverted accidental formatting of a library code

--- scripts/format_project_files_cxx.sh
+++ scripts/format_project_files_cxx.sh
@@ -4,7 +4,7 @@ PREV_CWD=$(pwd)
 cd "$(dirname "$0")"
 cd ..
 
-find . concur_storage core_tests crypto_tests \
+find . concur_storage core_tests \
   -maxdepth 1 \
   -iname '*.h' -o -iname '*.c' -o -iname '*.cpp' -o -iname '*.hpp' |
   xargs clang-format -style=file -i -fallback-style=none
