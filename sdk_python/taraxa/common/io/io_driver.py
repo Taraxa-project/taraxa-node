e315ea25877b17191441c19c2b1d4b15bab53d63
Oleh L <02p01r@gmail.com>
2019-08-06 22:19:24 +0300

    Fixed error logging

--- sdk_python/taraxa/common/io/io_driver.py
+++ sdk_python/taraxa/common/io/io_driver.py
@@ -5,7 +5,9 @@ from subprocess import Popen, PIPE
 class IODriver(ABC):
 
     def __init__(self, **popen_args):
-        self.popen_args = dict(encoding='utf-8', stdin=PIPE, stdout=PIPE, stderr=PIPE, **popen_args)
+        self.popen_args = dict(encoding='utf-8', stdin=PIPE, stdout=PIPE,
+                               # stderr=PIPE, TODO proper error handling
+                               **popen_args)
 
     def spawn(self, *args):
         return Popen(args, **self.popen_args)
