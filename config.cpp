8dc4a476e4f9c94c0ff94b1e1c427748c5a9911d
Mm <miho.frankovic@gmail.com>
2019-08-07 10:28:05 +0200

    Adding fixed timestamp in genesis

--- config.cpp
+++ config.cpp
@@ -47,8 +47,7 @@ FullNodeConfig::FullNodeConfig(std::string const &json_file)
       }
     }
     genesis_state = GenesisState::fromPtree(doc.get_child("genesis_state"));
-    genesis_state.block.sign(dev::Secret(node_secret,
-                              dev::Secret::ConstructFromStringType::FromHex));
+    genesis_state.block.updateHash();
   } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
