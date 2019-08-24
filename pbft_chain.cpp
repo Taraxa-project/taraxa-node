21b4ac7218ac0c48aacdaca6a1832f69719e7f79
gaoqi8649@gmail.com <gaoqi8649@gmail.com>
2019-08-23 19:42:22 -0700

    Add assert for duplicate DAG blocks in PBFT CS block

--- pbft_chain.cpp
+++ pbft_chain.cpp
@@ -611,7 +611,8 @@ uint64_t PbftChain::pushDagBlockHash(const taraxa::blk_hash_t& dag_block_hash) {
     // The DAG block already exist
     LOG(log_err_) << "Duplicate DAG block " << dag_block_hash
                   << " in pbft CS block";
-    return dag_blocks_map_[dag_block_hash];
+    assert(false);
+    //return dag_blocks_map_[dag_block_hash];
   }
   // push DAG block hash into array. DAG genesis at index 0
   dag_blocks_order_.emplace_back(dag_block_hash);
