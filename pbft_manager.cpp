8e2f2cc0dabf1d985e1435a8a580a1ccd0d8d7be
gaoqi8649@gmail.com <gaoqi8649@gmail.com>
2019-09-27 17:39:33 -0700

    Move updating DAG blocks order to finalize CS block from finalize pivot block

--- pbft_manager.cpp
+++ pbft_manager.cpp
@@ -1188,31 +1188,19 @@ bool PbftManager::pushPbftBlockIntoChain_(PbftBlock const &pbft_block) {
   PbftBlockTypes next_pbft_block_type = pbft_chain_->getNextPbftBlockType();
   if (next_pbft_block_type == pivot_block_type) {
     if (pbft_chain_->pushPbftPivotBlock(pbft_block)) {
+      // NOTICE: DAG may not have the dag block yet. This's OK since will do
+      // checking in CS block later.
       // reset proposed PBFT block hash to False for next CS block proposal
       proposed_block_hash_ = std::make_pair(NULL_BLOCK_HASH, false);
       LOG(log_inf_) << "Successful push pbft anchor block "
                     << pbft_block.getBlockHash() << " into chain! in round "
                     << pbft_round_;
-      // get dag blocks order
-      blk_hash_t dag_block_hash = pbft_block.getPivotBlock().getDagBlockHash();
-      uint64_t current_period;
-      std::shared_ptr<vec_blk_t> dag_blocks_order;
-      std::tie(current_period, dag_blocks_order) =
-          full_node->getDagBlockOrder(dag_block_hash);
-
       // Finalize pbft pivot block
       LOG(log_sil_) << full_node->getAddress()
                     << " Finalize pivot block in period "
                     << pbft_chain_->getPbftChainPeriod() << " round "
-                    << pbft_round_ << " step " << pbft_step_
-                    << " pivot: " << dag_block_hash;
-
-      // update DAG blocks order and DAG blocks table
-      for (auto const &dag_blk_hash : *dag_blocks_order) {
-        auto block_number = pbft_chain_->pushDagBlockHash(dag_blk_hash);
-        full_node->newOrderedBlock(dag_blk_hash, block_number);
-      }
-
+                    << pbft_round_ << " step " << pbft_step_ << " pivot: "
+                    << pbft_block.getPivotBlock().getDagBlockHash();
       return true;
     }
   } else if (next_pbft_block_type == schedule_block_type) {
@@ -1221,26 +1209,32 @@ bool PbftManager::pushPbftBlockIntoChain_(PbftBlock const &pbft_block) {
         LOG(log_inf_) << "Successful push pbft schedule block "
                       << pbft_block.getBlockHash() << " into chain! in round "
                       << pbft_round_;
-
-        // set DAG blocks period
+        // get dag blocks order
         blk_hash_t last_pivot_block_hash = pbft_chain_->getLastPbftPivotHash();
-        PbftBlock last_pivot_block =
-            pbft_chain_->getPbftBlockInChain(last_pivot_block_hash);
+        PbftBlock last_pivot_block = pbft_chain_->getPbftBlockInChain(
+            last_pivot_block_hash);
         blk_hash_t dag_block_hash =
             last_pivot_block.getPivotBlock().getDagBlockHash();
+        uint64_t current_period;
+        std::shared_ptr<vec_blk_t> dag_blocks_order;
+        std::tie(current_period, dag_blocks_order) =
+            full_node->getDagBlockOrder(dag_block_hash);
+        // update DAG blocks order and DAG blocks table
+        for (auto const &dag_blk_hash : *dag_blocks_order) {
+          auto block_number = pbft_chain_->pushDagBlockHash(dag_blk_hash);
+          full_node->newOrderedBlock(dag_blk_hash, block_number);
+        }
+        // set DAG blocks period
         uint64_t current_pbft_chain_period =
             last_pivot_block.getPivotBlock().getPeriod();
-
         uint dag_ordered_blocks_size = full_node->setDagBlockOrder(
             dag_block_hash, current_pbft_chain_period);
-
         // Finalize pbft concurent schedule block
         LOG(log_sil_) << full_node->getAddress()
                       << " Finalize cs block in period "
                       << current_pbft_chain_period << " round " << pbft_round_
                       << " step " << pbft_step_
                       << " anchor: " << dag_block_hash;
-
         // execute schedule block
         // TODO: VM executor will not take sortition_account_balance_table as
         //  reference. But will return a list of modified accounts as
