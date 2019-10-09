a9bd43eabd4de81330799071578d2df76695f62d
Oleh L <02p01r@gmail.com>
2019-10-09 22:49:21 +0300

    Added an iterator api to the DB interface

--- simple_db_face.hpp
+++ simple_db_face.hpp
@@ -10,6 +10,8 @@ const h256 TEMP_GENESIS_HASH = h256{}; /* genesisHash, put h256{} for now */
 
 class SimpleDBFace {
  public:
+  using foreach_fn =
+      std::function<bool(std::string const &, std::string const &)>;
   using sharedLock = boost::shared_lock<boost::shared_mutex>;
   using upgradableLock = boost::upgrade_lock<boost::shared_mutex>;
   using upgradeLock = boost::upgrade_to_unique_lock<boost::shared_mutex>;
@@ -26,7 +28,7 @@ class SimpleDBFace {
   virtual dev::bytes get(const h256 &key) = 0;
   virtual bool exists(const h256 &key) = 0;
   virtual void commit() = 0;
-  virtual void forEach(std::function<bool(std::string, std::string)> f) = 0;
+  virtual void forEach(foreach_fn const &f) = 0;
   virtual ~SimpleDBFace() = default;
 
  protected:
