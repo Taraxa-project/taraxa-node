e23cfc18b9e82ba29274d0e3becf4f7591944acb
Mm <miho.frankovic@gmail.com>
2019-10-27 09:14:36 +0100

    Removing virtual methods where not needed

--- database_face_cache.hpp
+++ database_face_cache.hpp
@@ -10,35 +10,35 @@ class DatabaseFaceCache {
                     uint32_t memory_cache_size = 1000)
       : db_(std::move(db)), memory_cache_size_(memory_cache_size), memory_cache_(memory_cache_size, memory_cache_size / 100) {}
 
-  virtual std::string lookup(dev::db::Slice _key) const;
-  virtual bool exists(dev::db::Slice _key) const;
-  virtual void insert(dev::db::Slice _key, dev::db::Slice _value);
+  std::string lookup(dev::db::Slice _key) const;
+  bool exists(dev::db::Slice _key) const;
+  void insert(dev::db::Slice _key, dev::db::Slice _value);
   
   inline dev::db::Slice toSlice(dev::bytes const& _b) const
   {
       return dev::db::Slice(reinterpret_cast<char const*>(&_b[0]), _b.size());
   }
 
-  virtual dev::bytes lookup(dev::h256 _key) const {
+  dev::bytes lookup(dev::h256 _key) const {
     return dev::asBytes(lookup(toSlice(_key.asBytes())));
   }
 
-  virtual bool exists(dev::h256 _key) const {
+  bool exists(dev::h256 _key) const {
     return exists(toSlice(_key.asBytes()));
   }
 
-  virtual void insert(dev::h256 _key, dev::bytes _value) {
+  void insert(dev::h256 _key, dev::bytes _value) {
     insert(toSlice(_key.asBytes()), toSlice(_value));
   }
   
-  virtual std::unique_ptr<dev::db::WriteBatchFace> createWriteBatch();
-  virtual void commit(std::unique_ptr<dev::db::WriteBatchFace> _batch);
+  std::unique_ptr<dev::db::WriteBatchFace> createWriteBatch();
+  void commit(std::unique_ptr<dev::db::WriteBatchFace> _batch);
 
   // A database must implement the `forEach` method that allows the caller
   // to pass in a function `f`, which will be called with the key and value
   // of each record in the database. If `f` returns false, the `forEach`
   // method must return immediately.
-  virtual void forEach(
+  void forEach(
       std::function<bool(dev::db::Slice, dev::db::Slice)> f) const;
 
  private:
