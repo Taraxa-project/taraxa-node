fe09c9a80c984bb139dcb39bd2618f1669f596e6
Oleh L <02p01r@gmail.com>
2019-12-23 23:16:02 +0400

    Removed a conflicting static initialization

--- core_tests/util.hpp
+++ core_tests/util.hpp
@@ -3,9 +3,11 @@
 
 #include <gtest/gtest.h>
 #include <libdevcrypto/Common.h>
+
 #include <boost/filesystem.hpp>
 #include <string>
 #include <type_traits>
+
 #include "config.hpp"
 
 namespace taraxa::core_tests::util {
@@ -14,26 +16,27 @@ using namespace dev;
 using namespace boost::filesystem;
 
 inline path const config_dir = "./core_tests/conf";
-inline auto const all_configs = []() {
-  vector<FullNodeConfig> ret;
-  for (auto& entry : recursive_directory_iterator(config_dir)) {
-    if (is_regular_file(entry)) {
-      ret.emplace_back(entry.path().string());
-    }
-  }
-  return move(ret);
-}();
-
-inline void cleanAllTestDB() {
-  cout << "Removing database directories" << endl;
-  for (auto& cfg : all_configs) {
-    remove_all(cfg.db_path);
-  }
-}
 
 template <typename T = ::testing::Test,
           typename = enable_if_t<is_base_of<::testing::Test, T>::value>>
-struct DBUsingTest : public T {
+struct DBUsingTest : T {
+  vector<FullNodeConfig> all_configs;
+
+  DBUsingTest() {
+    for (auto& entry : recursive_directory_iterator(config_dir)) {
+      if (is_regular_file(entry)) {
+        ret.emplace_back(entry.path().string());
+      }
+    }
+  }
+
+  void cleanAllTestDB() {
+    cout << "Removing database directories" << endl;
+    for (auto& cfg : all_configs) {
+      remove_all(cfg.db_path);
+    }
+  }
+
   void SetUp() override {
     cleanAllTestDB();
     T::SetUp();
