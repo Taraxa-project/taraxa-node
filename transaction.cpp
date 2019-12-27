dbba516bcbcafceaeb13bb0d82850911fdfbcbc0
Oleh L <02p01r@gmail.com>
2019-12-27 19:10:56 +0400

    Move init code out for the "start"

--- transaction.cpp
+++ transaction.cpp
@@ -464,21 +464,21 @@ void TransactionManager::setFullNode(std::shared_ptr<FullNode> full_node) {
   full_node_ = full_node;
   db_ = full_node->getDB();
   trx_qu_.setFullNode(full_node);
+  auto trx_count = db_->getStatusField(taraxa::StatusDbField::TrxCount);
+  trx_count_.store(trx_count);
+  DagBlock blk;
+  string pivot;
+  std::vector<std::string> tips;
+  full_node->getLatestPivotAndTips(pivot, tips);
+  DagFrontier frontier;
+  frontier.pivot = blk_hash_t(pivot);
+  updateNonce(blk, frontier);
 }
 
 void TransactionManager::start() {
   if (bool b = true; !stopped_.compare_exchange_strong(b, !b)) {
     return;
   }
-  auto trx_count = db_->getStatusField(taraxa::StatusDbField::TrxCount);
-  trx_count_.store(trx_count);
-  DagBlock blk;
-  string pivot;
-  std::vector<std::string> tips;
-  full_node_.lock()->getLatestPivotAndTips(pivot, tips);
-  DagFrontier frontier;
-  frontier.pivot = blk_hash_t(pivot);
-  updateNonce(blk, frontier);
   trx_qu_.start();
 }
 
