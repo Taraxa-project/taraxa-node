2faf320ca44bef7de8b676e0e504df27000618e7
Oleh <47611208+02p01r@users.noreply.github.com>
2020-06-02 07:23:50 +0400

    Rlp fixes (see the related taraxa-aleth changes) (#526)
    * upd taraxa-aleth
    
    * upd taraxa-aleth
    
    * make dag_block timestamp uint64

--- dag_block.hpp
+++ dag_block.hpp
@@ -68,7 +68,7 @@ class DagBlock {
 
   blk_hash_t getPivot() const { return pivot_; }
   level_t getLevel() const { return level_; }
-  int64_t getTimestamp() const { return timestamp_; }
+  auto getTimestamp() const { return timestamp_; }
   vec_blk_t getTips() const { return tips_; }
   vec_trx_t getTrxs() const { return trxs_; }
   sig_t getSig() const { return sig_; }
@@ -96,7 +96,7 @@ class DagBlock {
   vec_trx_t trxs_;  // transactions
   sig_t sig_;
   blk_hash_t hash_;
-  int64_t timestamp_ = -1;
+  uint64_t timestamp_ = 0;
   vdf_sortition::VdfSortition vdf_;
   mutable addr_t cached_sender_;  // block creater
 };
