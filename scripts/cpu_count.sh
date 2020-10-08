2aa4ad9f5e1593567305ad5535de3cc396f76744
Oleh Lukyrych <02p01r@gmail.com>
2020-10-08 12:30:14 +0400

    -

--- scripts/cpu_count.sh
+++ scripts/cpu_count.sh
@@ -1,4 +1,7 @@
 python -c "
 import multiprocessing
 print(multiprocessing.cpu_count())
-"
+" 2>/dev/null ||
+  getconf _NPROCESSORS_ONLN 2>/dev/null ||
+  getconf NPROCESSORS_ONLN 2>/dev/null ||
+  echo 1
