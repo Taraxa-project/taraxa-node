a00f87acf58cdbb8291af05c5654351dc729df37
Mm <miho.frankovic@gmail.com>
2020-11-06 16:52:37 +0100

    Increasing max line lengthto 150

--- src/util/lazy.hpp
+++ src/util/lazy.hpp
@@ -65,7 +65,7 @@ class Lazy {
   val_t const& operator*() const { return *operator->(); }
 
   operator val_t&() { return operator*(); }
-  operator val_t const&() const { return operator*(); }
+  operator val_t const &() const { return operator*(); }
 
   template <typename T>
   auto& operator[](T t) {
