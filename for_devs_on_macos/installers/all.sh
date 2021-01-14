f894dd7b1e14f2aaac2af3e0cc7b434008fdb0db
Oleh L <02p01r@gmail.com>
2021-01-14 16:23:43 +0400

    -

--- for_devs_on_macos/installers/all.sh
+++ for_devs_on_macos/installers/all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-find . -type f ! -path "./common.sh" -and ! -path "./all.sh" | parallel --will-cite -j999
+
+# running sequentially because it requests sudo rights
+./openssl.sh
+find . -type f ! -path "./common.sh" -and ! -path "./all.sh" -and ! -path "./openssl.sh" |
+  parallel --will-cite -j999
--- for_devs_on_macos/installers/openssl.sh
+++ for_devs_on_macos/installers/openssl.sh
@@ -4,4 +4,5 @@ source $(dirname "$0")/common.sh
 
 echo "installing openssl 1.1"
 brew install openssl@1.1
-cp -r $(brew --prefix openssl@1.1)/* ${dst}/
+echo "Running command as sudo..."
+sudo cp -r $(brew --prefix openssl@1.1)/* ${dst}/
