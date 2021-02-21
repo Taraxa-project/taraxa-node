b72be73fd38ec642519563c904f56e740ca6036c
Justin Snapp <justin.snapp@taraxa.io>
2021-02-21 09:20:36 -0800

    sync initiation debug

--- src/consensus/pbft_manager.cpp
+++ src/consensus/pbft_manager.cpp
@@ -1103,6 +1103,7 @@ bool PbftManager::comparePbftBlockScheduleWithDAGblocks_(PbftBlock const &pbft_b
   auto last_period = pbft_chain_->getPbftChainSize();
   LOG(log_nf_) << "DAG blocks have not sync yet. In period: " << last_period << ", anchor block hash " << anchor_hash
                << " is not found locally";
+  LOG(log_dg_) << "SYNCSTARTDEBUG state is finishing state: " << (state_ == finish_state) << " syncing_: " << capability_->syncing_ << " sync requested already this step: " << syncRequestedAlreadyThisStep_();
   if (state_ == finish_state && !have_executed_this_round_ && !capability_->syncing_ &&
       !syncRequestedAlreadyThisStep_()) {
     LOG(log_nf_) << "DAG blocks have not sync yet. In period: " << last_period << " PBFT block anchor: " << anchor_hash
