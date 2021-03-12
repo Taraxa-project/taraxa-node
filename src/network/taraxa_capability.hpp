5425553b9ade8dd7c598d4315ded7de4d5f4ad6c
02p01r <02p01r@gmail.com>
2021-03-12 05:02:36 +0400

    -

--- src/network/taraxa_capability.hpp
+++ src/network/taraxa_capability.hpp
@@ -218,10 +218,6 @@ class TaraxaCapability : public CapabilityFace, public Worker {
   std::unordered_map<NodeID, int> cnt_received_messages_;
   std::unordered_map<NodeID, int> test_sums_;
 
-  std::set<blk_hash_t> verified_blocks_;
-  std::condition_variable condition_for_verified_blocks_;
-  std::mutex mtx_for_verified_blocks;
-
   // Only used for testing without the full node set
   std::map<blk_hash_t, taraxa::DagBlock> test_blocks_;
   std::map<trx_hash_t, Transaction> test_transactions_;
