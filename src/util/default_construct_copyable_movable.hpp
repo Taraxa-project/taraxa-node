8e8f595ff146e2cbfef7c71759943f83bccd1e86
02p01r <02p01r@gmail.com>
2021-04-27 01:54:04 +0400

    -

--- src/util/default_construct_copyable_movable.hpp
+++ src/util/default_construct_copyable_movable.hpp
@@ -2,6 +2,11 @@
 
 #include <utility>
 
+// A wrapper for noncopyable/nonmovable types that resorts to default-constructing in copy/move constructors
+// and immutability in assignment operators
+// This basically captures a common work-around pattern for classes that contain fields of this kind of types
+// - a famous example would be a mutex type
+
 template <typename T>
 struct DefaultConstructCopyableMovable {
   T val;
