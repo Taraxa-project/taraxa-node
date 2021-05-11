a8bd5e7920c243d1797c6f650913f4904a984528
Henrique Santos Fernandes <sfrique@users.noreply.github.com>
2021-05-11 19:07:03 -0300

    Add sleep env var to hang exit to diagnose on k8s (#744)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 export TARAXA_CONF_PATH=${TARAXA_CONF_PATH:=/opt/taraxa/config.json}
 export TARAXA_PERSISTENT_PATH=${TARAXA_PERSISTENT_PATH:=/opt/taraxa/data}
 export TARAXA_COPY_COREDUMPS=${TARAXA_COPY_COREDUMPS:=true}
+export TARAXA_SLEEP_DIAGNOSE=${TARAXA_SLEEP_DIAGNOSE:=false}
 
 case $1 in
 
@@ -51,4 +52,15 @@ esac
 if [ "$TARAXA_COPY_COREDUMPS" = true ] ; then
     echo "Copying dump (if any) to $TARAXA_PERSISTENT_PATH"
     find / -maxdepth 1 -type f -name '*core*' -exec cp -v "{}" $TARAXA_PERSISTENT_PATH  \;
+fi
+
+# Hack to sleep forever so devs can diagnose the pod on k8s
+# We should not set Liveness/Readiness for this to work
+if [ "$TARAXA_SLEEP_DIAGNOSE" = true ] ; then
+    echo "Sleeping forever for diagnosis"
+    while true
+    do
+        echo "Crashed. Waiting on diagnosis..."
+        sleep 300
+    done
 fi
