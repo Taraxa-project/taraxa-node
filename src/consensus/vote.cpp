b667b306a45f7f2f2e5a77750ff45ef180ff0b86
Justin Snapp <justin.snapp@taraxa.io>
2021-05-17 01:32:00 -0700

    cleanup

--- src/consensus/vote.cpp
+++ src/consensus/vote.cpp
@@ -376,8 +376,8 @@ void VoteManager::cleanupVotes(uint64_t pbft_round) {
           max_received_round_for_address_[voter_account_address] = vote_round;
         } else {
           if (max_received_round_for_address_[voter_account_address] > vote_round + 1) {
-            rit->second.erase(v.first);
             remove_unverified_votes_hash.emplace_back(v.first);
+            rit->second.erase(v.first);
           } else if (vote_round > max_received_round_for_address_[voter_account_address]) {
             max_received_round_for_address_[voter_account_address] = vote_round;
           }
