ad5e5dbd5977b2fbc3eb697331f6430a6aac8e4f
02p01r <02p01r@gmail.com>
2021-06-18 20:55:28 +0400

    run clang-format

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -215,7 +215,7 @@ class StatusTable {
 };  // namespace taraxa
 
 template <typename... TS>
-std::string fmt(const std::string &pattern, const TS &...args) {
+std::string fmt(const std::string &pattern, const TS &... args) {
   return (boost::format(pattern) % ... % args).str();
 }
 
