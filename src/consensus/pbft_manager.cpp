491a6ac1d78d4482377a07b62087596704b8dac0
Justin Snapp <justin.snapp@taraxa.io>
2021-08-19 18:05:16 -0700

    fix(pbft): update cert voted state regardless of whether actuall place votes due to sortition

--- src/consensus/pbft_manager.cpp
+++ src/consensus/pbft_manager.cpp
@@ -911,21 +911,23 @@ void PbftManager::certifyBlock_() {
 
       if (executed_soft_voted_block_for_this_round || unverified_soft_vote_block_for_this_round_is_valid) {
         // generate cert vote
+        
+        // comparePbftBlockScheduleWithDAGblocks_ has checked the cert voted block exist
+
+        last_cert_voted_value_ = soft_voted_block_for_this_round_.first;
+        auto cert_voted_block = pbft_chain_->getUnverifiedPbftBlock(last_cert_voted_value_);
+
+        auto batch = db_->createWriteBatch();
+        db_->addPbftMgrVotedValueToBatch(PbftMgrVotedValue::last_cert_voted_value, last_cert_voted_value_, batch);
+        db_->addPbftCertVotedBlockToBatch(*cert_voted_block, batch);
+        db_->commitWriteBatch(batch);
+
+        should_have_cert_voted_in_this_round_ = true;
+        
         auto place_votes = placeVote_(soft_voted_block_for_this_round_.first, cert_vote_type, round, step_);
         if (place_votes) {
           LOG(log_nf_) << "Cert votes " << place_votes << " voting " << soft_voted_block_for_this_round_.first
                        << " in round " << round;
-
-          // comparePbftBlockScheduleWithDAGblocks_ has checked the cert voted block exist
-          last_cert_voted_value_ = soft_voted_block_for_this_round_.first;
-          auto cert_voted_block = pbft_chain_->getUnverifiedPbftBlock(last_cert_voted_value_);
-
-          auto batch = db_->createWriteBatch();
-          db_->addPbftMgrVotedValueToBatch(PbftMgrVotedValue::last_cert_voted_value, last_cert_voted_value_, batch);
-          db_->addPbftCertVotedBlockToBatch(*cert_voted_block, batch);
-          db_->commitWriteBatch(batch);
-
-          should_have_cert_voted_in_this_round_ = true;
         }
       }
     }
@@ -987,7 +989,7 @@ void PbftManager::firstFinish_() {
           
         }
       }
-      
+
       auto place_votes = placeVote_(own_starting_value_for_round_, next_vote_type, round, step_);
       if (place_votes) {
         LOG(log_nf_) << "Next votes " << place_votes << " voting nodes own starting value "
