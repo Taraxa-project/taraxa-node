b480c03ca14f81c29c6a6dd87042eb2a5ad70e68
JakubFornadel <jakub.fornadel@gmail.com>
2021-10-11 15:03:14 +0200

    chore(threadpool): configure tp to have only 3 threads

--- libraries/config/include/config/config.hpp
+++ libraries/config/include/config/config.hpp
@@ -41,7 +41,7 @@ struct NetworkConfig {
   uint64_t network_id;
   uint16_t network_performance_log_interval = 0;
   uint16_t network_num_threads = std::max(uint(1), uint(std::thread::hardware_concurrency() / 2));
-  uint16_t network_packets_processing_threads = 10;
+  uint16_t network_packets_processing_threads = 3;
 };
 
 struct BlockProposerConfig {
