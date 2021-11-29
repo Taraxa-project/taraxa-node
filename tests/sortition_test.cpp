444a7e3471de7f7a399abb8b2bd4b239ce57e268
Dmitry Kostenko <kostello.d@gmail.com>
2021-11-29 12:13:32 +0300

    chore: add test showing incorrect order

--- tests/sortition_test.cpp
+++ tests/sortition_test.cpp
@@ -242,6 +242,21 @@ TEST_F(SortitionTest, average_correction_per_percent) {
   EXPECT_EQ(sp2.averageCorrectionPerPercent(), sp.averageCorrectionPerPercent());
 }
 
+TEST_F(SortitionTest, db_keys_order) {
+  auto db = std::make_shared<DbStorage>(data_dir / "db");
+
+  auto batch = db->createWriteBatch();
+  for (uint16_t i = 1; i <= 1000; ++i) {
+    db->saveSortitionParamsChange(i, {i, i, {}}, batch);
+  }
+  db->commitWriteBatch(batch);
+
+  const auto params_from_db = db->getLastSortitionParams(1000);
+  for (const auto& e : params_from_db) {
+    std::cout << "params for period" << e.period << std::endl;
+  }
+}
+
 TEST_F(SortitionTest, params_changes_from_db) {
   auto db = std::make_shared<DbStorage>(data_dir / "db");
 
