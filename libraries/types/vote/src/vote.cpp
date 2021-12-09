9ccb7ac87a853f509fd2b9d5d7dc8799ec0939fc
gaoqi8649@gmail.com <gaoqi8649@gmail.com>
2021-12-09 15:55:59 -0800

    fix: add binary search for binominal distribution

--- libraries/types/vote/src/vote.cpp
+++ libraries/types/vote/src/vote.cpp
@@ -33,16 +33,28 @@ bytes VrfPbftSortition::getRlpBytes() const {
 
 uint64_t VrfPbftSortition::getBinominalDistribution(uint64_t stake, double dpos_total_votes_count, double threshold,
                                                     const uint512_t& output) {
+  if (!stake) return 0;  // Stake is 0
+
   auto l = static_cast<uint512_t>(output).convert_to<boost::multiprecision::mpfr_float>();
   auto division = l / kMax512bFP;
   double ratio = division.convert_to<double>();
   boost::math::binomial_distribution<double> dist(static_cast<double>(stake), threshold / dpos_total_votes_count);
-  for (uint64_t j = 0; j < stake; ++j) {
-    // Found the correct boundary, break
-    if (ratio <= cdf(dist, j)) {
-      return j;
+
+  // Binary search for find the lowest stake <= cdf
+  uint64_t start = 0;
+  uint64_t end = stake - 1;
+  while (start + 1 < end) {
+    auto mid = start + (end - start) / 2;
+    auto target = cdf(dist, mid);
+    if (ratio <= target) {
+      end = mid;
+    } else {
+      start = mid;
     }
   }
+  // Found the correct boundary
+  if (ratio <= cdf(dist, start)) return start;
+  if (ratio <= cdf(dist, end)) return end;
   return stake;
 }
 
