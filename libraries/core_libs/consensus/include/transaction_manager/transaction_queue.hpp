fb277895abc55980191c72439200545c5a0f8c33
Matus Kysel <matuskysel1@gmail.com>
2022-01-31 14:08:29 +0100

    chore: code review

--- libraries/core_libs/consensus/include/transaction_manager/transaction_queue.hpp
+++ libraries/core_libs/consensus/include/transaction_manager/transaction_queue.hpp
@@ -3,17 +3,62 @@
 #include "transaction/transaction.hpp"
 
 namespace taraxa {
-
+/**
+ * @brief this is NOT thread safe class. It is proteced only by transactions_mutex_ in the transaction mamanger !!!
+ *
+ */
 class TransactionQueue {
  public:
   TransactionQueue();
 
+  /**
+   * @brief insert a transaction into the queue, sorted by priority
+   *
+   * @param transaction
+   * @return true
+   * @return false
+   */
   bool insert(const std::shared_ptr<Transaction>& transaction);
+
+  /**
+   * @brief removed transaction from queue
+   *
+   * @param hash
+   * @return true
+   * @return false
+   */
   bool erase(const trx_hash_t& hash);
 
+  /**
+   * @brief returns the transaction or null
+   *
+   * @param hash
+   * @return std::shared_ptr<Transaction>
+   */
   std::shared_ptr<Transaction> get(const trx_hash_t& hash) const;
+
+  /**
+   * @brief returns up to the number of requested transaction sorted by priority
+   *
+   * @param count
+   * @return std::vector<std::shared_ptr<Transaction>>
+   */
   std::vector<std::shared_ptr<Transaction>> get(uint64_t count = 0) const;
+
+  /**
+   * @brief returns true/false if the transaction is in the queue
+   *
+   * @param hash
+   * @return true
+   * @return false
+   */
   bool contains(const trx_hash_t& hash) const;
+
+  /**
+   * @brief returns size of priority queue
+   *
+   * @return size_t
+   */
   size_t size() const;
 
  private:
@@ -28,7 +73,7 @@ class TransactionQueue {
   };
   // It has to be multiset as two trx could have same value (nonce and gas price)
   using PriorityQueue = std::multiset<std::shared_ptr<Transaction>, PriorityCompare>;
-  PriorityQueue prio_queue_;
+  PriorityQueue priority_queue_;
   std::unordered_map<trx_hash_t, PriorityQueue::iterator> hash_queue_;
   std::unordered_map<addr_t, std::map<u256, PriorityQueue::iterator>> nonce_queue_;
 };
--- libraries/core_libs/consensus/src/transaction_manager/transaction_queue.cpp
+++ libraries/core_libs/consensus/src/transaction_manager/transaction_queue.cpp
@@ -2,7 +2,7 @@
 
 namespace taraxa {
 
-TransactionQueue::TransactionQueue() : prio_queue_{PriorityCompare{*this}} {}
+TransactionQueue::TransactionQueue() : priority_queue_{PriorityCompare{*this}} {}
 
 size_t TransactionQueue::size() const { return hash_queue_.size(); }
 
@@ -16,15 +16,15 @@ std::shared_ptr<Transaction> TransactionQueue::get(const trx_hash_t &hash) const
 }
 
 std::vector<std::shared_ptr<Transaction>> TransactionQueue::get(uint64_t count) const {
-  if (count == 0 || prio_queue_.size() <= count) {
-    std::vector<std::shared_ptr<Transaction>> ret(prio_queue_.begin(), prio_queue_.end());
+  if (count == 0 || priority_queue_.size() <= count) {
+    std::vector<std::shared_ptr<Transaction>> ret(priority_queue_.begin(), priority_queue_.end());
     return ret;
   }
 
   std::vector<std::shared_ptr<Transaction>> ret;
   ret.reserve(count);
-  uint16_t counter = 0;
-  for (const auto &trx : prio_queue_) {
+  size_t counter = 0;
+  for (const auto &trx : priority_queue_) {
     if (counter == count) break;
     ret.push_back(trx);
     counter++;
@@ -39,24 +39,26 @@ bool TransactionQueue::erase(const trx_hash_t &hash) {
 
   // Remove trx from nonce map
   const auto nonce_it = nonce_queue_.find((*it->second)->getSender());
-  assert(nonce_it != nonce_queue_.end());
   nonce_it->second.erase((*it->second)->getNonce());
   // Remove even address from nonce map
   if (nonce_it->second.empty()) nonce_queue_.erase(nonce_it);
 
   // Clean the rest
-  prio_queue_.erase(it->second);
+  priority_queue_.erase(it->second);
   hash_queue_.erase(it);
   return true;
 }
 
 bool TransactionQueue::insert(const std::shared_ptr<Transaction> &transaction) {
+  assert(transaction);
   if (hash_queue_.contains(transaction->getHash())) return false;
   // First we need to insert it in nonce map, so compare works fine
   auto nonce_it = nonce_queue_[transaction->getSender()].emplace(
       std::make_pair(transaction->getNonce(), PriorityQueue::iterator()));
-  const auto it = prio_queue_.insert(transaction);
-  assert(it != prio_queue_.end());
+  const auto it = priority_queue_.insert(transaction);
+  // This assert is here to check if PriorityCompare works correctly. If object is not inserted, then there could be
+  // something wrong with comparator
+  assert(it != priority_queue_.end());
   nonce_it.first->second = it;
   hash_queue_[transaction->getHash()] = it;
   return true;
