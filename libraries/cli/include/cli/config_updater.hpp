d0c70d007e8b3d4f8d0aa94696eae9bbf97804d0
Matus Kysel <matuskysel1@gmail.com>
2022-02-01 12:23:15 +0100

    chore: simplified version of config updater

--- libraries/cli/include/cli/config_updater.hpp
+++ libraries/cli/include/cli/config_updater.hpp
@@ -6,24 +6,6 @@
 
 namespace taraxa::cli {
 
-class ConfigChange {
- public:
-  ConfigChange() = default;
-  virtual ~ConfigChange() = default;
-
-  virtual bool ShouldApply(uint32_t version) const = 0;
-  virtual void Apply(Json::Value& old_conf, const Json::Value& new_conf) const = 0;
-};
-
-class NetworkIPChange : public ConfigChange {
- public:
-  NetworkIPChange() = default;
-  virtual ~NetworkIPChange() = default;
-
-  bool ShouldApply(uint32_t version) const;
-  void Apply(Json::Value& old_conf, const Json::Value& new_conf) const;
-};
-
 class ConfigUpdater {
  public:
   ConfigUpdater(int network_id);
@@ -32,8 +14,16 @@ class ConfigUpdater {
   void UpdateConfig(const std::string& config_name, Json::Value& old_conf);
 
  private:
+  struct ConfigChange {
+    ConfigChange() = default;
+    ConfigChange(auto&& version_function, auto&& apply_function)
+        : should_apply(version_function), apply(apply_function) {}
+    std::function<bool(uint32_t version)> should_apply;
+    std::function<void(Json::Value& old_conf, const Json::Value& new_conf)> apply;
+  };
+
   Json::Value new_conf_;
-  std::vector<std::unique_ptr<ConfigChange>> config_changes_;
+  std::vector<ConfigChange> config_changes_;
 };
 
 }  // namespace taraxa::cli
--- libraries/cli/src/config_updater.cpp
+++ libraries/cli/src/config_updater.cpp
@@ -6,25 +6,25 @@
 namespace taraxa::cli {
 
 //// NetworkIPChange
-bool NetworkIPChange::ShouldApply(uint32_t version) const {
+auto NetworkIPChangeShouldApply = [](uint32_t version) {
   if (version < 1) return true;
   return false;
-}
+};
 
-void NetworkIPChange::Apply(Json::Value& old_conf, const Json::Value& new_conf) const {
+auto NetworkIPChangeApply = [](Json::Value& old_conf, const Json::Value& new_conf) {
   if (!old_conf["network_address"].isNull()) {
     old_conf["network_listen_ip"] = old_conf["network_address"];
     old_conf.removeMember("network_address");
   } else if (old_conf["network_listen_ip"].isNull()) {
     old_conf["network_listen_ip"] = new_conf["network_listen_ip"];
   }
-}
+};
 //// NetworkIPChange
 
 ConfigUpdater::ConfigUpdater(int network_id) {
-  new_conf_ = Tools::generateConfig((Config::NetworkIdType)network_id);
-  // Regiser changes that should applay
-  config_changes_.push_back(std::make_unique<NetworkIPChange>());
+  new_conf_ = Tools::generateConfig(static_cast<Config::NetworkIdType>(network_id));
+  // Regiser changes that should apply
+  config_changes_.emplace_back(NetworkIPChangeShouldApply, NetworkIPChangeApply);
 }
 
 void ConfigUpdater::UpdateConfig(const std::string& config_name, Json::Value& old_conf) {
@@ -33,8 +33,8 @@ void ConfigUpdater::UpdateConfig(const std::string& config_name, Json::Value& ol
     version = old_conf["version"].asUInt();
   }
   for (const auto& change : config_changes_) {
-    if (change->ShouldApply(version)) {
-      change->Apply(old_conf, new_conf_);
+    if (change.should_apply(version)) {
+      change.apply(old_conf, new_conf_);
     }
   }
   old_conf["version"] = TARAXA_CONFIG_VERSION;
