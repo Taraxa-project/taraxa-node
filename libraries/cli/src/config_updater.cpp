602c5d36c29fc38677d966d74d72aff112d34f02
Matus Kysel <matuskysel1@gmail.com>
2022-02-02 14:38:12 +0100

    chore: removed config version

--- libraries/cli/src/config_updater.cpp
+++ libraries/cli/src/config_updater.cpp
@@ -6,12 +6,7 @@
 namespace taraxa::cli {
 
 //// NetworkIPChange
-auto NetworkIPChangeShouldApply = [](uint32_t version) {
-  if (version < 1) return true;
-  return false;
-};
-
-auto NetworkIPChangeApply = [](Json::Value& old_conf, const Json::Value& new_conf) {
+auto NetworkIPChange = [](Json::Value& old_conf, const Json::Value& new_conf) {
   if (!old_conf["network_address"].isNull()) {
     old_conf["network_listen_ip"] = old_conf["network_address"];
     old_conf.removeMember("network_address");
@@ -24,20 +19,13 @@ auto NetworkIPChangeApply = [](Json::Value& old_conf, const Json::Value& new_con
 ConfigUpdater::ConfigUpdater(int network_id) {
   new_conf_ = Tools::generateConfig(static_cast<Config::NetworkIdType>(network_id));
   // Regiser changes that should apply
-  config_changes_.emplace_back(NetworkIPChangeShouldApply, NetworkIPChangeApply);
+  config_changes_.emplace_back(NetworkIPChange);
 }
 
 void ConfigUpdater::UpdateConfig(const std::string& config_name, Json::Value& old_conf) {
-  uint32_t version = 0;
-  if (!old_conf["version"].isNull()) {
-    version = old_conf["version"].asUInt();
-  }
   for (const auto& change : config_changes_) {
-    if (change.should_apply(version)) {
-      change.apply(old_conf, new_conf_);
-    }
+    change.apply(old_conf, new_conf_);
   }
-  old_conf["version"] = TARAXA_CONFIG_VERSION;
   Tools::writeJsonToFile(config_name, old_conf);
 }
 
