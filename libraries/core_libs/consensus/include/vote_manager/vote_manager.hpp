9fd6afd629143d9bc610a8a9617a179aed84ff67
gaoqi8649@gmail.com <gaoqi8649@gmail.com>
2022-05-26 21:55:41 -0700

    chore: add comments for vote module

--- libraries/core_libs/consensus/include/vote_manager/vote_manager.hpp
+++ libraries/core_libs/consensus/include/vote_manager/vote_manager.hpp
@@ -12,30 +12,89 @@ namespace taraxa {
 
 class Network;
 
+/**
+ * @brief NextVotesManager class manage next voting type votes for previous PBFT round.
+ *
+ * Node could send previous PBFT round next voting type votes to help peers catch up to the current PBFT round. Also,
+ * node could receive enough next voting type votes from peers,  to help node itself catch up to the correct PBFT round
+ * in consensus.
+ */
 class NextVotesManager {
  public:
   NextVotesManager(addr_t node_addr, std::shared_ptr<DbStorage> db, std::shared_ptr<FinalChain> final_chain);
 
+  /**
+   * @brief Clear previous PBFT round next voting type votes
+   */
   void clear();
 
+  /**
+   * @brief Check if the vote has been in the map
+   * @param next_vote_hash next voting type vote hash
+   * @return true if found
+   */
   bool find(vote_hash_t next_vote_hash);
 
+  /**
+   * @brief Check if exist enough next voting type votes
+   * @return true if there are enough next voting type votes
+   */
   bool enoughNextVotes() const;
 
+  /**
+   * @brief Check if exist enough next voting type votes for PBFT NULL block hash
+   * @return true if there are enough next voting type votes
+   */
   bool haveEnoughVotesForNullBlockHash() const;
 
+  /**
+   * @brief Get next voting type votes vote value
+   * @return next voting type votes vote value
+   */
   blk_hash_t getVotedValue() const;
 
+  /**
+   * @brief Get previous PBFT round all next voting type votes
+   * @return previous PBFT round all next voting type votes
+   */
   std::vector<std::shared_ptr<Vote>> getNextVotes();
 
+  /**
+   * @brief Get total weight of previous PBFT round all next voting type votes
+   * @return total weight of previous PBFT round all next voting type votes
+   */
   size_t getNextVotesWeight() const;
 
+  /**
+   * @brief Add a bunch of next voting type votes to the map
+   * @param next_votes next voting type votes
+   * @param pbft_2t_plus_1 PBFT 2t+1 is 2/3 of PBFT sortition threshold and plus 1
+   */
   void addNextVotes(std::vector<std::shared_ptr<Vote>> const& next_votes, size_t pbft_2t_plus_1);
 
+  /**
+   * @brief Update a bunch of next voting type votes to the map
+   * @param next_votes next voting type votes
+   * @param pbft_2t_plus_1 PBFT 2t+1 is 2/3 of PBFT sortition threshold and plus 1
+   */
   void updateNextVotes(std::vector<std::shared_ptr<Vote>> const& next_votes, size_t pbft_2t_plus_1);
 
+  /**
+   * @brief Update a bunch of next voting type votes that synced from peers to the map
+   * @param votes next voting type votes
+   * @param pbft_2t_plus_1 PBFT 2t+1 is 2/3 of PBFT sortition threshold and plus 1
+   */
   void updateWithSyncedVotes(std::vector<std::shared_ptr<Vote>>& votes, size_t pbft_2t_plus_1);
 
+  /**
+   * @brief Verify vote validation
+   * @param vote vote
+   * @param dpos_period DPOS period
+   * @param dpos_total_votes_count total DPOS votes count
+   * @param pbft_sortition_threshold PBFT sortition threshold is minimum of between PBFT committee size and total DPOS
+   * votes count
+   * @return true if passed validation
+   */
   bool voteVerification(std::shared_ptr<Vote>& vote, uint64_t dpos_period, size_t dpos_total_votes_count,
                         size_t pbft_sortition_threshold);
 
@@ -45,6 +104,11 @@ class NextVotesManager {
   using UpgradableLock = boost::upgrade_lock<boost::shared_mutex>;
   using UpgradeLock = boost::upgrade_to_unique_lock<boost::shared_mutex>;
 
+  /**
+   * @brief Assertion happens when there is more than 1 voting value for a non-NULL block hash
+   * @param next_votes_1 a bunch of next voting type votes vote on a non-NULL block hash
+   * @param next_votes_2 a bunch of next voting type votes vote on another non-NULL block hash
+   */
   void assertError_(std::vector<std::shared_ptr<Vote>> next_votes_1,
                     std::vector<std::shared_ptr<Vote>> next_votes_2) const;
 
@@ -64,6 +128,9 @@ class NextVotesManager {
   LOG_OBJECTS_DEFINE
 };
 
+/**
+ * @brief VoteManager class manage votes for PBFT consensus
+ */
 class VoteManager {
  public:
   VoteManager(const addr_t& node_addr, std::shared_ptr<DbStorage> db, std::shared_ptr<FinalChain> final_chain,
@@ -74,39 +141,144 @@ class VoteManager {
   VoteManager& operator=(const VoteManager&) = delete;
   VoteManager& operator=(VoteManager&&) = delete;
 
+  /**
+   * @brief Set network as a weak pointer
+   * @param network a weak pinter
+   */
   void setNetwork(std::weak_ptr<Network> network);
 
   // Unverified votes
+  /**
+   * @brief Add a vote to the unverified votes map
+   * @param vote vote
+   * @return true if added
+   */
   bool addUnverifiedVote(std::shared_ptr<Vote> const& vote);
+
+  /**
+   * @brief Add verified votes to the unverified votes map
+   * @param votes verified votes
+   */
   void moveVerifyToUnverify(std::vector<std::shared_ptr<Vote>> const& votes);
+
+  /**
+   * @brief Remove an unverified vote
+   * @param pbft_round vote PBFT round
+   * @param vote_hash vote hash
+   */
   void removeUnverifiedVote(uint64_t pbft_round, vote_hash_t const& vote_hash);
+
+  /**
+   * @brief Check if the vote has been in the unverified votes map
+   * @param pbft_round vote PBFT round
+   * @param vote_hash vote hash
+   * @return true if exist
+   */
   bool voteInUnverifiedMap(uint64_t pbft_round, vote_hash_t const& vote_hash);
+
+  /**
+   * @brief Get all unverified votes
+   * @return all unverified votes
+   */
   std::vector<std::shared_ptr<Vote>> getUnverifiedVotes();
+
+  /**
+   * @brief Clear the unverified votes map
+   */
   void clearUnverifiedVotesTable();
+
+  /**
+   * @brief Get the total size of all unverified votes
+   * @return the total size of all unverified votes
+   */
   uint64_t getUnverifiedVotesSize() const;
 
   // Verified votes
+  /**
+   * @brief Add a vote to the verified votes map
+   * @param vote vote
+   */
   void addVerifiedVote(std::shared_ptr<Vote> const& vote);
+
+  /**
+   * @brief Check if the vote has been in the verified votes map
+   * @param vote vote
+   * @return true if exist
+   */
   bool voteInVerifiedMap(std::shared_ptr<Vote> const& vote);
+
+  /**
+   * @brief Clear the verified votes map
+   */
   void clearVerifiedVotesTable();
+
+  /**
+   * @brief Get all verified votes
+   * @return all verified votes
+   */
   std::vector<std::shared_ptr<Vote>> getVerifiedVotes();
+
+  /**
+   * @brief Get the total size of all verified votes
+   * @return the total size of all verified votes
+   */
   uint64_t getVerifiedVotesSize() const;
 
+  /**
+   * @brief Move all verified votes to the unverified map. Since PBFT chain has pushed new blocks, that will affect DPOS
+   * eligible votes count and players' eligibility
+   */
   void removeVerifiedVotes();
 
+  /**
+   * @brief Cleanup votes for previous PBFT rounds and verify all unverified votes
+   * @param pbft_round current PBFT round
+   * @param is_valid vote validation function
+   */
   void verifyVotes(uint64_t pbft_round, std::function<bool(std::shared_ptr<Vote> const&)> const& is_valid);
 
+  /**
+   * @brief Cleanup votes for previous PBFT rounds
+   * @param pbft_round current PBFT round
+   */
   void cleanupVotes(uint64_t pbft_round);
 
+  /**
+   * @brief Get a bunch of votes in string format
+   * @param votes votes
+   * @return a bunch of votes in string format
+   */
   std::string getJsonStr(std::vector<std::shared_ptr<Vote>> const& votes);
 
+  /**
+   * @brief Get all verified votes in proposal vote type for the current PBFT round
+   * @param pbft_round current PBFT round
+   * @return all verified votes in proposal vote type for the current PBFT round
+   */
   std::vector<std::shared_ptr<Vote>> getProposalVotes(uint64_t pbft_round);
 
+  /**
+   * @brief Get a bunch of votes that vote on the same voting value in the specific PBFT round and step, the total votes
+   * weights must be greater or equal to PBFT 2t+1
+   * @param round PBFT round
+   * @param step PBFT step
+   * @param two_t_plus_one PBFT 2t+1 is 2/3 of PBFT sortition threshold and plus 1
+   * @return VotesBundle a bunch of votes that vote on the same voting value in the specific PBFT round and step
+   */
   std::optional<VotesBundle> getVotesBundleByRoundAndStep(uint64_t round, size_t step, size_t two_t_plus_one);
 
+  /**
+   * @brief Check if there are enough next voting type votes to set PBFT to a forward round
+   * @param two_t_plus_one PBFT 2t+1 is 2/3 of PBFT sortition threshold and plus 1
+   * @return a new PBFT round number if there are enough next voting type votes. Otherwise return 0
+   */
   uint64_t roundDeterminedFromVotes(size_t two_t_plus_one);
 
  private:
+  /**
+   * @brief Retrieve all verified votes from DB to the verified votes map. And broadcast all next voting type votes to
+   * peers if node has extended the partition steps (1000). That only happens when nodes start.
+   */
   void retreieveVotes_();
 
   using UniqueLock = boost::unique_lock<boost::shared_mutex>;
--- libraries/types/vote/include/vote/vrf_sortition.hpp
+++ libraries/types/vote/include/vote/vrf_sortition.hpp
@@ -8,12 +8,23 @@
 
 namespace taraxa {
 
+/** @addtogroup Vote
+ * @{
+ */
+
 enum PbftVoteTypes : uint8_t { propose_vote_type = 0, soft_vote_type, cert_vote_type, next_vote_type };
 
+/**
+ * @brief VrfPbftMsg struct uses vote type, PBFT round, and PBFT step to generate a message for doing VRF sortition.
+ */
 struct VrfPbftMsg {
   VrfPbftMsg() = default;
   VrfPbftMsg(PbftVoteTypes type, uint64_t round, size_t step) : type(type), round(round), step(step) {}
 
+  /**
+   * @brief Combine vote type, PBFT round, and PBFT step to a string
+   * @return a string of vote type, PBFT round, and PBFT step
+   */
   std::string toString() const {
     return std::to_string(type) + "_" + std::to_string(round) + "_" + std::to_string(step);
   }
@@ -30,6 +41,10 @@ struct VrfPbftMsg {
     return strm;
   }
 
+  /**
+   * @brief Get bytes of RLP stream
+   * @return bytes of RLP stream
+   */
   bytes getRlpBytes() const {
     dev::RLPStream s;
     s.appendList(3);
@@ -44,10 +59,17 @@ struct VrfPbftMsg {
   size_t step;
 };
 
+/**
+ * @brief HashableVrf struct uses VRF output, voter address, and vote weight index to calculate a hash number.
+ */
 struct HashableVrf {
   HashableVrf(const vrf_wrapper::vrf_output_t& vrf, const public_t& addr, uint64_t i = 0)
       : output(vrf), address(addr), iter(i) {}
 
+  /**
+   * @brief Get a hash number of combining VRF output, voter address, and vote weight index
+   * @return a hash number in Secure Hash Algorithm 3
+   */
   dev::h256 getHash() const {
     dev::RLPStream s;
     s.appendList(3);
@@ -62,6 +84,9 @@ struct HashableVrf {
   uint64_t iter;
 };
 
+/**
+ * @brief VrfPbftSortition class used for doing VRF sortition to place a vote or to propose a new PBFT block
+ */
 class VrfPbftSortition : public vrf_wrapper::VrfSortitionBase {
   using vrf_sk_t = vrf_wrapper::vrf_sk_t;
   using vrf_pk_t = vrf_wrapper::vrf_pk_t;
@@ -76,8 +101,16 @@ class VrfPbftSortition : public vrf_wrapper::VrfSortitionBase {
 
   explicit VrfPbftSortition(dev::bytes const& rlp);
 
+  /**
+   * @brief Get bytes of RLP stream
+   * @return bytes of RLP stream
+   */
   dev::bytes getRlpBytes() const;
 
+  /**
+   * @brief Verify VRF sortition
+   * @return true if passed
+   */
   bool verify() const { return VrfSortitionBase::verify(pbft_msg_.getRlpBytes()); }
 
   bool operator==(VrfPbftSortition const& other) const {
@@ -87,9 +120,25 @@ class VrfPbftSortition : public vrf_wrapper::VrfSortitionBase {
   static inline uint256_t max256bits = std::numeric_limits<uint256_t>::max();
   static inline auto kMax256bFP = max256bits.convert_to<boost::multiprecision::mpfr_float>();
 
+  /**
+   * @brief Calculate a vote weight in binominal distribution
+   * @param stake voter DPOS eligible votes count
+   * @param dpos_total_votes_count total DPOS votes count
+   * @param threshold PBFT sortition threshold that is minimum of between PBFT committee size and total DPOS votes count
+   * @param hash a hash number of combining VRF output, voter address, and vote weight index
+   * @return vote weight
+   */
   static uint64_t getBinominalDistribution(uint64_t stake, double dpos_total_votes_count, double threshold,
                                            const uint256_t& hash);
 
+  /**
+   * @brief Calculate vote weight
+   * @param stake voter DPOS eligible votes count
+   * @param dpos_total_votes_count total DPOS votes count
+   * @param threshold PBFT sortition threshold that is minimum of between PBFT committee size and total DPOS votes count
+   * @param address voter public key
+   * @return vote weight
+   */
   uint64_t calculateWeight(uint64_t stake, double dpos_total_votes_count, double threshold,
                            const public_t& address) const;
 
@@ -104,5 +153,7 @@ class VrfPbftSortition : public vrf_wrapper::VrfSortitionBase {
 
   VrfPbftMsg pbft_msg_;
 };
+
+/** @}*/
 
 }  // namespace taraxa
