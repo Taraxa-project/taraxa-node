cba0224605a18341c4f34008d674e4cc313e98a2
Matus Kysel <matuskysel1@gmail.com>
2022-06-29 15:44:18 +0200

    chore: introduce dynamic encoding

--- libraries/core_libs/consensus/include/final_chain/contract_interface.hpp
+++ libraries/core_libs/consensus/include/final_chain/contract_interface.hpp
@@ -1,88 +1,72 @@
 #pragma once
 
+#include <libdevcore/Common.h>
+
 #include <limits>
 
 #include "common/types.hpp"
 #include "final_chain/final_chain.hpp"
+#include "final_chain/rewards_stats.hpp"
 
 namespace taraxa::final_chain {
 class ContractInterface {
  public:
-  ContractInterface(addr_t contract_addr, std::shared_ptr<FinalChain> final_chain);
-  ~ContractInterface() = default;
-
-  template <typename Result, typename... Params>
-  Result call(const string& function, const Params&... args);
-
-  template <typename Result>
-  static Result unpack(bytes const& data);
-
-  template <typename T, std::enable_if_t<std::numeric_limits<T>::is_integer>* = nullptr>
-  static auto unpack(bytes& data, T& num) {
-    num = fromBigEndian<T>(data);
-  }
-
   /// PACKING ///
   template <typename... Params>
-  static bytes pack(const string& function, const Params&... args) {
-    bytes data = getFunction(function);
-    pack(data, args...);
-    return data;
+  static bytes packFunctionCall(const string& function, const Params&... args) {
+    auto output = getFunction(function);
+    auto args_data = pack(args...);
+    output.insert(output.end(), std::make_move_iterator(args_data.begin()), std::make_move_iterator(args_data.end()));
+    return output;
   }
 
   template <typename Param, typename... Params>
-  static void pack(bytes& data, const Param& arg, const Params&... args) {
-    pack(data, arg);
-    pack(data, args...);
+  static bytes pack(const Param& arg, const Params&... args) {
+    auto output = pack(arg);
+    auto data = pack(args...);
+    output.insert(output.end(), std::make_move_iterator(data.begin()), std::make_move_iterator(data.end()));
+    return output;
   }
 
   template <typename T, std::enable_if_t<std::numeric_limits<T>::is_integer>* = nullptr>
-  static void pack(bytes& data, T num) {
+  static bytes pack(T num) {
+    bytes data;
     boost::multiprecision::uint256_t big_num{num};
     auto tmp = dev::toBigEndian(big_num);
     data.insert(data.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
+    return data;
   }
 
-  static void pack(bytes& data, const addr_t& addr) {
+  static bytes pack(const addr_t& addr) {
+    bytes data;
     data.insert(data.end(), 12, 0);
     data.insert(data.end(), addr.begin(), addr.end());
+    return data;
   }
 
-  static void pack(bytes& data, bool flag) {
+  static bytes pack(bool flag) {
+    bytes data;
     boost::multiprecision::uint256_t big_num{flag};
     auto tmp = dev::toBigEndian(big_num);
     data.insert(data.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
+    return data;
   }
 
-  static void pack(bytes& data, const bytes& value) {
-    assert(value.size() <= 32);
+  static bytes pack(const bytes& value) {
+    bytes data;
+    // encode length of msg
+    if (value.size()) {
+      data = pack(value.size());
+    }
+    // encode msg
     data.insert(data.end(), value.begin(), value.end());
-    data.insert(data.end(), 32 - value.size(), 0);
-  }
-
-  template <typename T>
-  static void pack(bytes& data, const std::vector<T>& array) {
-    pack(data, array.size());
-    for (const auto& value : array) {
-      pack(data, value);
-    }
-  }
-
-  template <typename T, std::size_t N>
-  static void pack(bytes& data, const std::array<T, N>& array) {
-    for (const auto& value : array) {
-      pack(data, value);
-    }
+    // trailing zeros
+    data.insert(data.end(), 32 - (value.size() % 32), 0);
+    return data;
   }
   ////////////
 
-  static bytes getFunction(const string& function);
-
- private:
-  bytes finalChainCall(bytes&& data);
-
-  const addr_t contract_addr_;
-  std::shared_ptr<FinalChain> final_chain_;
+  static bytes getFunction(const string& function) { return dev::sha3(function.c_str()).ref().cropped(0, 4).toBytes(); }
 };
 
 }  // namespace taraxa::final_chain
