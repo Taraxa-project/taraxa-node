8f26800d7ffc7ae7c408288ed9caf6635df72872
Matus Kysel <matuskysel1@gmail.com>
2022-06-29 16:59:18 +0200

    chore: finish with dynamic encoding for function calls

--- libraries/core_libs/consensus/include/final_chain/contract_interface.hpp
+++ libraries/core_libs/consensus/include/final_chain/contract_interface.hpp
@@ -20,11 +20,31 @@ class ContractInterface {
     return output;
   }
 
-  template <typename Param, typename... Params>
-  static bytes pack(const Param& arg, const Params&... args) {
-    auto output = pack(arg);
-    auto data = pack(args...);
-    output.insert(output.end(), std::make_move_iterator(data.begin()), std::make_move_iterator(data.end()));
+  template <typename... Params>
+  static bytes pack(const Params&... args) {
+    bytes output;
+    bytes offset_data;
+    // initial offset is after last arg
+    auto current_offset = kWordSize * sizeof...(args);
+    (
+        [&] {
+          auto data = pack(args);
+          const auto data_size = data.size();
+          // Dynamic encoding needs to be appended to the end of the whole structure
+          if (data_size > kWordSize) {
+            offset_data.reserve(offset_data.size() + data_size);
+            offset_data.insert(offset_data.end(), std::make_move_iterator(data.begin()),
+                               std::make_move_iterator(data.end()));
+            // Save position where data will be stored
+            data = pack(current_offset);
+            current_offset += data_size;
+          }
+          output.insert(output.end(), std::make_move_iterator(data.begin()), std::make_move_iterator(data.end()));
+        }(),
+        ...);
+
+    output.insert(output.end(), std::make_move_iterator(offset_data.begin()),
+                  std::make_move_iterator(offset_data.end()));
     return output;
   }
 
@@ -61,12 +81,14 @@ class ContractInterface {
     // encode msg
     data.insert(data.end(), value.begin(), value.end());
     // trailing zeros
-    data.insert(data.end(), 32 - (value.size() % 32), 0);
+    data.insert(data.end(), kWordSize - (value.size() % kWordSize), 0);
     return data;
   }
   ////////////
 
   static bytes getFunction(const string& function) { return dev::sha3(function.c_str()).ref().cropped(0, 4).toBytes(); }
+
+  const static size_t kWordSize = 32;
 };
 
 }  // namespace taraxa::final_chain
