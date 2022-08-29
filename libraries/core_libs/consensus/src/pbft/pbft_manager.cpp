a57ae6c2f8b02b29396e839337399c5c2ccb834c
JakubFornadel <jakub.fornadel@gmail.com>
2022-08-29 11:41:56 +0200

    chore: fix typo

--- libraries/core_libs/consensus/src/pbft/pbft_manager.cpp
+++ libraries/core_libs/consensus/src/pbft/pbft_manager.cpp
@@ -544,7 +544,7 @@ void PbftManager::initialState() {
   next_step_time_ms_ = 0;
 
   updateDposState_();
-  // Initializetwo_t_plus_one_and sortition_threshold
+  // Initialize two_t_plus_one_and sortition_threshold
   updateTwoTPlusOneAndThreshold_();
 
   if (round > 1) {
