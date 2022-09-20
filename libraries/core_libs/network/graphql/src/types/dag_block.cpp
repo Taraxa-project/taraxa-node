7cd9f4fcd5065d3449a2c528353f009a343285ef
Matus Kysel <matuskysel1@gmail.com>
2022-09-20 16:08:08 +0200

    chore: fix crash on account

--- libraries/core_libs/network/graphql/src/types/dag_block.cpp
+++ libraries/core_libs/network/graphql/src/types/dag_block.cpp
@@ -48,9 +48,7 @@ response::Value DagBlock::getTimestamp() const noexcept {
   return response::Value(static_cast<int>(dag_block_->getTimestamp()));
 }
 
-int DagBlock::getTransactionCount() const noexcept {
-  return static_cast<int>(dag_block_->getTrxs().size());
-}
+int DagBlock::getTransactionCount() const noexcept { return static_cast<int>(dag_block_->getTrxs().size()); }
 
 std::optional<std::vector<std::shared_ptr<object::Transaction>>> DagBlock::getTransactions() const noexcept {
   std::vector<std::shared_ptr<object::Transaction>> transactions_result;
