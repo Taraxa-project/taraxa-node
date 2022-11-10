f109b4df83c53da99fbe623f93b1e46e6f7c3df3
mfrankovi <34503697+mfrankovi@users.noreply.github.com>
2022-11-10 13:50:35 +0000

    Deploying to gh-pages from @ Taraxa-project/taraxa-node@cd99539d124b7ed70ee05aa5c11a32a22cf2a2ba 🚀

--- group___d_a_g.js
+++ group___d_a_g.js
@@ -23,7 +23,7 @@ var group___d_a_g =
       [ "db_", "group___d_a_g.html#a35a01524cd913a3c6109def68b32f842", null ],
       [ "executor_", "group___d_a_g.html#a7ac36f60bce997dbb4f952aaa0b1e22a", null ],
       [ "key_manager_", "group___d_a_g.html#a1adea10b5c0d6646f18ef7d4f710af9d", null ],
-      [ "last_frontier_", "group___d_a_g.html#a3dd257da790b6978fd814dcce1785c5a", null ],
+      [ "last_propose_level_", "group___d_a_g.html#abce138565674ce4912b612feee18453d", null ],
       [ "max_num_tries_", "group___d_a_g.html#aae1e3fb89b672481721fd3bc4180d3f9", null ],
       [ "num_tries_", "group___d_a_g.html#a0120111247e61dad6da6541c54fc47d9", null ],
       [ "trx_mgr_", "group___d_a_g.html#ab211aaaf09fe7fd2602ee0b35f86b9e8", null ]
