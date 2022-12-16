50f4a3ab2521fcda6c5a1a167d877e54f826bffa
Matus Kysel <matuskysel1@gmail.com>
2022-12-16 10:49:48 +0100

    chore: fix on final_chain crash

--- libraries/core_libs/consensus/src/pbft/pbft_manager.cpp
+++ libraries/core_libs/consensus/src/pbft/pbft_manager.cpp
@@ -1173,7 +1173,12 @@ std::shared_ptr<PbftBlock> PbftManager::generatePbftBlock(PbftPeriod propose_per
                  [](const auto &v) { return v->getHash(); });
   h256 last_state_root;
   if (propose_period > config_.state_root_recording_delay) {
-    last_state_root = final_chain_->block_header(propose_period - config_.state_root_recording_delay)->state_root;
+    if (const auto header = final_chain_->block_header(propose_period - config_.state_root_recording_delay)) {
+      last_state_root = header->state_root;
+    } else {
+      LOG(log_wr_) << "Block for period " << propose_period << " could not be proposed as we are behind";
+      return {};
+    }
   }
   return std::make_shared<PbftBlock>(prev_blk_hash, anchor_hash, order_hash, last_state_root, propose_period,
                                      node_addr_, node_sk_, std::move(reward_votes_hashes));
@@ -1413,11 +1418,15 @@ bool PbftManager::validatePbftBlock(const std::shared_ptr<PbftBlock> &pbft_block
   auto const &pbft_block_hash = pbft_block->getBlockHash();
 
   auto period = pbft_block->getPeriod();
-
   {
     h256 prev_state_root_hash;
     if (period > config_.state_root_recording_delay) {
-      prev_state_root_hash = final_chain_->block_header(period - config_.state_root_recording_delay)->state_root;
+      if (const auto header = final_chain_->block_header(period - config_.state_root_recording_delay)) {
+        prev_state_root_hash = header->state_root;
+      } else {
+        LOG(log_wr_) << "Block " << pbft_block_hash << " could not be validated as we are behind";
+        return false;
+      }
     }
     if (pbft_block->getPrevStateRoot() != prev_state_root_hash) {
       LOG(log_er_) << "Block " << pbft_block_hash << " state root " << pbft_block->getPrevStateRoot()
