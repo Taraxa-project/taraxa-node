3006b34364ce04551ff31f8293714743282ae968
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-04-18 10:47:20 -0700

    chore: send v1 period data rlp in v1 sync packet handler

--- libraries/core_libs/network/src/v1_tarcap/packet_handlers/get_pbft_sync_packet_handler.cpp
+++ libraries/core_libs/network/src/v1_tarcap/packet_handlers/get_pbft_sync_packet_handler.cpp
@@ -78,12 +78,35 @@ void OmgGetPbftSyncPacketHandler::sendPbftBlocks(dev::p2p::NodeID const &peer_id
       assert(false);
     }
 
+    // Transform period data rlp from v2 to v1
+    auto transformPeriodDataRlpToV1 = [](const dev::bytes &period_data_v2) -> dev::bytes {
+      // Create PeriodData old(v1) rlp format
+      PeriodData period_data(period_data_v2);
+
+      dev::RLPStream period_data_rlp(PeriodData::kRlpItemCount);
+      period_data_rlp.appendRaw(period_data.pbft_blk->rlp(true));
+      period_data_rlp.appendList(period_data.previous_block_cert_votes.size());
+      for (auto const &v : period_data.previous_block_cert_votes) {
+        period_data_rlp.appendRaw(v->rlp(true));
+      }
+      period_data_rlp.appendList(period_data.dag_blocks.size());
+      for (auto const &b : period_data.dag_blocks) {
+        period_data_rlp.appendRaw(b.rlp(true));
+      }
+      period_data_rlp.appendList(period_data.transactions.size());
+      for (auto const &t : period_data.transactions) {
+        period_data_rlp.appendRaw(t->rlp());
+      }
+
+      return period_data_rlp.invalidate();
+    };
+
     dev::RLPStream s;
-    if (pbft_chain_synced && last_block) {
+    if (pbft_chain_synced && last_block && block_period > 1) {
       s.appendList(3);
       s << last_block;
-      // TODO: once PeriodData db migration is done, parse PeriodData and create old rlp structure
-      s.appendRaw(data);
+      s.appendRaw(transformPeriodDataRlpToV1(data));
+
       // Latest finalized block cert votes are saved in db as reward votes for new blocks
       const auto votes = vote_mgr_->getRewardVotes();
       s.appendList(votes.size());
@@ -93,8 +116,7 @@ void OmgGetPbftSyncPacketHandler::sendPbftBlocks(dev::p2p::NodeID const &peer_id
     } else {
       s.appendList(2);
       s << last_block;
-      // TODO: once PeriodData db migration is done, parse PeriodData and create old rlp structure
-      s.appendRaw(data);
+      s.appendRaw(transformPeriodDataRlpToV1(data));
     }
     LOG(log_dg_) << "Sending PbftSyncPacket period " << block_period << " to " << peer_id;
     sealAndSend(peer_id, tarcap::SubprotocolPacketType::PbftSyncPacket, std::move(s));
