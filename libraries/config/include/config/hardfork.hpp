67d2a66b8110f1de80a36889665832527f929ac6
Dmytro Kostenko <kostello.d@gmail.com>
2023-05-09 12:39:19 +0200

    feat: add option to change rewards distribution frequency with hardfork

--- libraries/config/include/config/hardfork.hpp
+++ libraries/config/include/config/hardfork.hpp
@@ -5,7 +5,18 @@
 #include "common/encoding_rlp.hpp"
 
 struct Hardforks {
-  uint64_t fix_genesis_fork_block = 0;
+  /*
+   * @brief key is block number at which change is applied and value is new distribution interval.
+   * Default distribution frequency is every block
+   * To change rewards distribution frequency we should add a new element in map below.
+   * For example {{101, 20}, {201, 10}} means:
+   * 1. for blocks [1,100] we are distributing rewards every block
+   * 2. for blocks [101, 200] rewards are distributed every 20 block. On blocks 120, 140, etc.
+   * 3. for blocks after 201 rewards are distributed every 10 block. On blocks 210, 220, etc.
+   */
+  using RewardsDistributionMap = std::map<uint64_t, uint32_t>;
+  RewardsDistributionMap rewards_distribution_frequency;
+
   HAS_RLP_FIELDS
 };
 
