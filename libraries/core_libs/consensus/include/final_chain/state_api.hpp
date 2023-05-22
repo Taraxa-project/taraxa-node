8de18c5e7d43a62131f82ca122a9c586b3510742
Dmytro Kostenko <kostello.d@gmail.com>
2023-05-22 14:36:41 +0200

    feat: implement hardfork to change fee rewards distribution

--- libraries/core_libs/consensus/include/final_chain/state_api.hpp
+++ libraries/core_libs/consensus/include/final_chain/state_api.hpp
@@ -19,8 +19,10 @@ class StateAPI {
   std::function<h256(EthBlockNumber)> get_blk_hash_;
   taraxa_evm_GetBlockHash get_blk_hash_c_;
   taraxa_evm_state_API_ptr this_c_;
-  dev::RLPStream rlp_enc_transition_state_;
-  StateTransitionResult result_buf_transition_state_;
+  dev::RLPStream rlp_enc_execution_result_;
+  TransactionsExecutionResult result_buf_execution_result_;
+  dev::RLPStream rlp_enc_rewards_distribution_;
+  RewardsDistributionResult result_buf_rewards_distribution_;
   string db_path_;
 
  public:
@@ -41,10 +43,12 @@ class StateAPI {
   bytes trace(EthBlockNumber blk_num, const EVMBlock& blk, const std::vector<EVMTransaction> trx,
               std::optional<Tracing> params = {}) const;
   StateDescriptor get_last_committed_state_descriptor() const;
-  const StateTransitionResult& transition_state(const EVMBlock& block,
-                                                const util::RangeView<EVMTransaction>& transactions,
-                                                const std::vector<rewards::BlockStats>& rewards_stats = {});
+
+  const TransactionsExecutionResult& execute_transactions(const EVMBlock& block,
+                                                          const util::RangeView<EVMTransaction>& transactions);
+  const RewardsDistributionResult& distribute_rewards(const std::vector<rewards::BlockStats>& rewards_stats);
   void transition_state_commit();
+
   void create_snapshot(PbftPeriod period);
   void prune(const std::vector<dev::h256>& state_root_to_keep, EthBlockNumber blk_num);
 
