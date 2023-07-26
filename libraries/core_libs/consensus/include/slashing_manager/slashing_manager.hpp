544837656017286997ed54a40866001d61f42d50
Matus Kysel <matuskysel1@gmail.com>
2023-07-26 12:56:06 +0200

    feat: implement prototype of slashing manager

---
+++ libraries/core_libs/consensus/include/slashing_manager/slashing_manager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "final_chain/final_chain.hpp"
+#include "transaction/gas_pricer.hpp"
+#include "transaction/transaction_manager.hpp"
+
+namespace taraxa {
+class SlashingManager {
+ public:
+  SlashingManager(std::shared_ptr<FinalChain> final_chain, std::shared_ptr<TransactionManager> trx_manager,
+                  std::shared_ptr<GasPricer> gas_pricer, uint64_t chain_id, secret_t node_sk);
+  SlashingManager(const SlashingManager &) = delete;
+  SlashingManager(SlashingManager &&) = delete;
+  SlashingManager &operator=(const SlashingManager &) = delete;
+  SlashingManager &operator=(SlashingManager &&) = delete;
+
+  bool submit_double_voting(const std::shared_ptr<Vote> &vote_a, const std::shared_ptr<Vote> &vote_b) const;
+
+ private:
+  std::shared_ptr<FinalChain> final_chain_;
+  std::shared_ptr<TransactionManager> trx_manager_;
+  std::shared_ptr<GasPricer> gas_pricer_;
+  const uint64_t kChainId;
+  const addr_t kAddress;
+  const secret_t kPrivateKey;
+};
+}  // namespace taraxa
