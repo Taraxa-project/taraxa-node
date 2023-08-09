4cd3a6ef0db265263c20fa4d3f16f99a2b1097dc
Matus Kysel <matuskysel1@gmail.com>
2023-08-09 09:54:29 +0200

    feat: introduce redelegation map

--- libraries/config/include/config/hardfork.hpp
+++ libraries/config/include/config/hardfork.hpp
@@ -4,8 +4,13 @@
 
 #include "common/encoding_rlp.hpp"
 
+using RedelegationMap = std::map<taraxa::addr_t, taraxa::addr_t>;
+Json::Value enc_json(const RedelegationMap& obj);
+void dec_json(const Json::Value& json, RedelegationMap& obj);
+
 struct Hardforks {
   uint64_t fix_redelegate_block_num = -1;
+  RedelegationMap redelegations;
   HAS_RLP_FIELDS
 };
 
--- libraries/config/src/hardfork.cpp
+++ libraries/config/src/hardfork.cpp
@@ -1,13 +1,29 @@
 #include "config/hardfork.hpp"
 
+Json::Value enc_json(const RedelegationMap& obj) {
+  Json::Value json(Json::objectValue);
+  for (auto const& [k, v] : obj) {
+    json[dev::toJS(k)] = dev::toJS(v);
+  }
+  return json;
+}
+
+void dec_json(const Json::Value& json, RedelegationMap& obj) {
+  for (const auto& k : json.getMemberNames()) {
+    obj[taraxa::addr_t(k)] = taraxa::addr_t(json[k].asString());
+  }
+}
+
 Json::Value enc_json(const Hardforks& obj) {
   Json::Value json(Json::objectValue);
   json["fix_redelegate_block_num"] = dev::toJS(obj.fix_redelegate_block_num);
+  json["redelegations"] = enc_json(obj.redelegations);
   return json;
 }
 
 void dec_json(const Json::Value& json, Hardforks& obj) {
   obj.fix_redelegate_block_num = dev::getUInt(json["fix_redelegate_block_num"]);
+  dec_json(json["redelegations"], obj.redelegations);
 }
 
-RLP_FIELDS_DEFINE(Hardforks, fix_redelegate_block_num)
+RLP_FIELDS_DEFINE(Hardforks, fix_redelegate_block_num, redelegations)
