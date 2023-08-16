c162e1f0237659f643e993af5a8a22f72741bff7
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-08-16 12:26:05 -0700

    commit double voting proof when received non-unique vote through packets

--- libraries/core_libs/network/src/tarcap/packets_handlers/latest/get_next_votes_bundle_packet_handler.cpp
+++ libraries/core_libs/network/src/tarcap/packets_handlers/latest/get_next_votes_bundle_packet_handler.cpp
@@ -8,10 +8,10 @@ namespace taraxa::network::tarcap {
 GetNextVotesBundlePacketHandler::GetNextVotesBundlePacketHandler(
     const FullNodeConfig &conf, std::shared_ptr<PeersState> peers_state,
     std::shared_ptr<TimePeriodPacketsStats> packets_stats, std::shared_ptr<PbftManager> pbft_mgr,
-    std::shared_ptr<PbftChain> pbft_chain, std::shared_ptr<VoteManager> vote_mgr, const addr_t &node_addr,
-    const std::string &logs_prefix)
+    std::shared_ptr<PbftChain> pbft_chain, std::shared_ptr<VoteManager> vote_mgr,
+    std::shared_ptr<SlashingManager> slashing_manager, const addr_t &node_addr, const std::string &logs_prefix)
     : ExtVotesPacketHandler(conf, std::move(peers_state), std::move(packets_stats), std::move(pbft_mgr),
-                            std::move(pbft_chain), std::move(vote_mgr), node_addr,
+                            std::move(pbft_chain), std::move(vote_mgr), std::move(slashing_manager), node_addr,
                             logs_prefix + "GET_NEXT_VOTES_BUNDLE_PH") {}
 
 void GetNextVotesBundlePacketHandler::validatePacketRlpFormat(const threadpool::PacketData &packet_data) const {
