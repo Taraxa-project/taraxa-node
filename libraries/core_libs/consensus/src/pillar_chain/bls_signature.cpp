27261db4fb1031c81845591a482170cbe385e9c3
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-09-18 17:50:25 -0700

    added bls siagntures syncing

--- libraries/core_libs/consensus/src/pillar_chain/bls_signature.cpp
+++ libraries/core_libs/consensus/src/pillar_chain/bls_signature.cpp
@@ -1,33 +1,40 @@
 #include "pillar_chain/bls_signature.hpp"
-#include "common/encoding_rlp.hpp"
+
 #include <libdevcore/SHA3.h>
+
+#include "common/encoding_rlp.hpp"
 
 namespace taraxa {
 
 BlsSignature::BlsSignature(const dev::RLP& rlp) {
-  // TODO: parse rlp
+  std::string sig_str, pk_str;
+  util::rlp_tuple(util::RLPDecoderRef(rlp, true), pillar_block_hash_, sig_str, pk_str);
+
+  std::stringstream(sig_str) >> signature_;
+  std::stringstream(pk_str) >> public_key_;
 }
 
-BlsSignature::BlsSignature(PillarBlock::Hash pillar_block_hash, const libff::alt_bn128_G2& public_key, const libff::alt_bn128_Fr& secret) {
-  pillar_block_hash_ = pillar_block_hash;
-  public_key_ = public_key;
-  signature_ = libBLS::Bls::CoreSignAggregated( pillar_block_hash_.toString(), secret );
-}
+BlsSignature::BlsSignature(PillarBlock::Hash pillar_block_hash, const libff::alt_bn128_G2& public_key,
+                           const libff::alt_bn128_Fr& secret)
+    : pillar_block_hash_(pillar_block_hash),
+      signature_(libBLS::Bls::CoreSignAggregated(getHash().toString(), secret)),
+      public_key_(public_key) {}
 
-bool BlsSignature::isValid() const {
-  return libBLS::Bls::CoreVerify( public_key_, pillar_block_hash_.toString(), signature_);
-}
+bool BlsSignature::isValid() const { return libBLS::Bls::CoreVerify(public_key_, getHash().toString(), signature_); }
 
-blk_hash_t BlsSignature::getPillarBlockHash() const {
-  return pillar_block_hash_;
-}
+blk_hash_t BlsSignature::getPillarBlockHash() const { return pillar_block_hash_; }
 
 dev::bytes BlsSignature::getRlp() const {
-  dev::RLPStream s(1);
+  dev::RLPStream s(3);
   s << pillar_block_hash_;
-  // TODO: add signature into rlp
-  //s << signature_;
-  //s << public_key_;
+
+  std::stringstream sig_ss;
+  sig_ss << signature_;
+  s << sig_ss.str();
+
+  std::stringstream pk_ss;
+  pk_ss << public_key_;
+  s << pk_ss.str();
 
   return s.invalidate();
 }
