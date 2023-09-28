2d5698d4fe83f658cea9690dc625938f5f6ba1b7
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-09-28 15:34:09 -0700

    implement address -> bls public key mapping

--- libraries/core_libs/consensus/src/key_manager/key_manager.cpp
+++ libraries/core_libs/consensus/src/key_manager/key_manager.cpp
@@ -3,21 +3,22 @@
 namespace taraxa {
 
 static const vrf_wrapper::vrf_pk_t kEmptyVrfKey;
+static const libff::alt_bn128_G2 kEmptyBlsKey;
 
 KeyManager::KeyManager(std::shared_ptr<FinalChain> final_chain) : final_chain_(std::move(final_chain)) {}
 
-std::shared_ptr<vrf_wrapper::vrf_pk_t> KeyManager::get(EthBlockNumber blk_n, const addr_t& addr) {
+std::shared_ptr<vrf_wrapper::vrf_pk_t> KeyManager::getVrfKey(EthBlockNumber blk_n, const addr_t& addr) {
   {
-    std::shared_lock lock(mutex_);
-    if (const auto it = key_map_.find(addr); it != key_map_.end()) {
+    std::shared_lock lock(vrf_keys_mutex_);
+    if (const auto it = vrf_keys_.find(addr); it != vrf_keys_.end()) {
       return it->second;
     }
   }
 
   try {
     if (auto key = final_chain_->dpos_get_vrf_key(blk_n, addr); key != kEmptyVrfKey) {
-      std::unique_lock lock(mutex_);
-      return key_map_.insert_or_assign(addr, std::make_shared<vrf_wrapper::vrf_pk_t>(std::move(key))).first->second;
+      std::unique_lock lock(vrf_keys_mutex_);
+      return vrf_keys_.insert_or_assign(addr, std::make_shared<vrf_wrapper::vrf_pk_t>(std::move(key))).first->second;
     }
   } catch (state_api::ErrFutureBlock&) {
     return nullptr;
@@ -25,4 +26,26 @@ std::shared_ptr<vrf_wrapper::vrf_pk_t> KeyManager::get(EthBlockNumber blk_n, con
 
   return nullptr;
 }
+
+std::shared_ptr<libff::alt_bn128_G2> KeyManager::getBlsKey(EthBlockNumber blk_n, const addr_t& addr) {
+  {
+    std::shared_lock lock(bls_keys_mutex_);
+    if (const auto it = bls_keys_.find(addr); it != bls_keys_.end()) {
+      return it->second;
+    }
+  }
+
+  try {
+    // TODO: does kEmptyBlsKey comparison work ???
+    if (auto key = final_chain_->dpos_get_bls_key(blk_n, addr); key != kEmptyBlsKey) {
+      std::unique_lock lock(bls_keys_mutex_);
+      return bls_keys_.insert_or_assign(addr, std::make_shared<libff::alt_bn128_G2>(std::move(key))).first->second;
+    }
+  } catch (state_api::ErrFutureBlock&) {
+    return nullptr;
+  }
+
+  return nullptr;
+}
+
 }  // namespace taraxa
--- libraries/core_libs/network/src/tarcap/packets_handlers/latest/common/ext_bls_sig_packet_handler.cpp
+++ libraries/core_libs/network/src/tarcap/packets_handlers/latest/common/ext_bls_sig_packet_handler.cpp
@@ -1,23 +1,50 @@
 #include "network/tarcap/packets_handlers/latest/common/ext_bls_sig_packet_handler.hpp"
-
+#include "key_manager/key_manager.hpp"
 #include "pillar_chain/pillar_chain_manager.hpp"
+#include "final_chain/final_chain.hpp"
 
 namespace taraxa::network::tarcap {
 
 ExtBlsSigPacketHandler::ExtBlsSigPacketHandler(const FullNodeConfig &conf, std::shared_ptr<PeersState> peers_state,
                                                std::shared_ptr<TimePeriodPacketsStats> packets_stats,
                                                std::shared_ptr<PillarChainManager> pillar_chain_manager,
+                                               std::shared_ptr<KeyManager> key_manager, std::shared_ptr<final_chain::FinalChain> final_chain,
                                                const addr_t &node_addr, const std::string &logs_prefix)
     : PacketHandler(conf, std::move(peers_state), std::move(packets_stats), node_addr, logs_prefix + "BLS_SIG_PH"),
-      pillar_chain_manager_{std::move(pillar_chain_manager)} {}
+      pillar_chain_manager_{std::move(pillar_chain_manager)},
+      key_manager_(std::move(key_manager)),
+      final_chain_(std::move(final_chain)){}
 
 void ExtBlsSigPacketHandler::processBlsSignature(const std::shared_ptr<BlsSignature> &signature,
                                                  const std::shared_ptr<TaraxaPeer> &peer) {
   if (!pillar_chain_manager_->isRelevantBlsSig(signature)) {
     LOG(log_dg_) << "Drop irrelevant signature " << signature->getHash() << " from peer " << peer->getId();
+    return;
   }
 
-  if (!signature->isValid()) {
+  const auto sig_period = signature->getPeriod();
+  const auto sig_author = signature->getSignerAddr();
+
+  // Check if signer registered his bls key
+  const auto bls_pub_key = key_manager_->getBlsKey(sig_period, sig_author);
+  if (!bls_pub_key) {
+    LOG(log_er_) << "No bls public key mapped for node " << sig_author << ". Signature " << signature->getHash();
+    return;
+  }
+
+  // Check if signer is eligible validator
+  try {
+    if (!final_chain_->dpos_is_eligible(sig_period, sig_author)) {
+      std::ostringstream err_msg;
+      err_msg << "Invalid signature " << signature->getHash() << " from peer " << peer->getId() << ". Signer is not eligible validator";
+      throw MaliciousPeerException(err_msg.str());
+    }
+  } catch (state_api::ErrFutureBlock& e) {
+    LOG(log_wr_) << "Signature " << signature->getHash() << " period " << sig_period << " is too far ahead of DPOS. " << e.what();
+    return;
+  }
+
+  if (!signature->isValid(bls_pub_key)) {
     std::ostringstream err_msg;
     err_msg << "Invalid signature " << signature->getHash() << " from peer " << peer->getId();
     throw MaliciousPeerException(err_msg.str());
