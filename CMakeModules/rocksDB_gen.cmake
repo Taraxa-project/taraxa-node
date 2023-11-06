5b3e834b095322cacec5402cfe0e806ca104876d
Matus Kysel <matuskysel1@gmail.com>
2023-11-06 20:57:46 +0100

    chore: add support for new rocksDB

---
+++ CMakeModules/rocksDB_gen.cmake
@@ -0,0 +1,30 @@
+# ========================================================================== #
+# RocksDB key-value store                                                    #
+# ========================================================================== #
+include(FetchContent)
+
+set(Boost_NO_WARN_NEW_VERSIONS 1)
+
+FetchContent_Declare(
+  rocksdb
+  GIT_REPOSITORY https://github.com/facebook/rocksdb
+  GIT_TAG v8.5.3
+  GIT_SHALLOW TRUE
+)
+
+FetchContent_GetProperties(rocksdb)
+
+message(STATUS "Populating rocksdb")
+set(USE_RTTI 1)
+set(WITH_LZ4 ON)
+set(WITH_TESTS OFF CACHE INTERNAL "")
+set(WITH_JNI OFF CACHE INTERNAL "")
+set(WITH_TOOLS OFF CACHE INTERNAL "")
+set(WITH_BENCHMARK_TOOLS  OFF CACHE INTERNAL "")
+set(WITH_CORE_TOOLS  OFF CACHE INTERNAL "")
+set(CMAKE_ENABLE_SHARED OFF CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=unused-const-variable -Wno-error=pedantic -Wno-error=format-pedantic -Wno-error=gnu-zero-variadic-macro-arguments -Wno-error=extra-semi -Wc++98-compat-extra-semi -Wno-error=deprecated-copy -Wno-deprecated-copy -Wno-error=pessimizing-move -Wno-pessimizing-move -Wno-pessimizing-move")
+
+FetchContent_Populate(rocksdb)
+add_subdirectory(${rocksdb_SOURCE_DIR} ${rocksdb_BINARY_DIR} EXCLUDE_FROM_ALL)
+ 
