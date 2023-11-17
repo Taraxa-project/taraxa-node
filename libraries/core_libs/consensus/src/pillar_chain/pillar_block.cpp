e501ac9020f879a86ee94e878dc30ad026e10d87
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-11-17 13:16:21 -0800

    add stakes_changes into pillar block

--- libraries/core_libs/consensus/src/pillar_chain/pillar_block.cpp
+++ libraries/core_libs/consensus/src/pillar_chain/pillar_block.cpp
@@ -6,18 +6,33 @@
 
 namespace taraxa {
 
+dev::bytes PillarBlock::ValidatorStakeChange::getRlp() const {
+  dev::RLPStream s(2);
+  s << addr;
+  s << stake;
+
+  return s.invalidate();
+}
+
 PillarBlock::PillarBlock(PbftPeriod period, blk_hash_t epoch_blocks_merkle_root,
+                         std::vector<ValidatorStakeChange>&& validator_stakes_changes,
                          PillarBlock::Hash previous_pillar_block_hash)
     : period_(period),
       epoch_blocks_merkle_root_(epoch_blocks_merkle_root),
+      validators_stakes_changes_(std::move(validator_stakes_changes)),
       previous_pillar_block_hash_(previous_pillar_block_hash),
       kCachedHash(dev::sha3(getRlp())) {}
 
 dev::bytes PillarBlock::getRlp() const {
-  dev::RLPStream s(3);
+  dev::RLPStream s(4);
   s << period_;
   s << epoch_blocks_merkle_root_;
   s << previous_pillar_block_hash_;
+
+  s.appendList(validators_stakes_changes_.size());
+  for (const auto& stake_change : validators_stakes_changes_) {
+    s.appendRaw(stake_change.getRlp());
+  }
 
   return s.invalidate();
 }
