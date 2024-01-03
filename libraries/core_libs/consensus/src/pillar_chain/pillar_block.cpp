3672530706b225197c772074e8d1d63082ed14d4
Jakub Fornadel <jakub.fornadel@gmail.com>
2024-01-03 14:59:24 -0800

    fix rlp encoding/decoding and db saving of pillar chain related data

--- libraries/core_libs/consensus/src/pillar_chain/pillar_block.cpp
+++ libraries/core_libs/consensus/src/pillar_chain/pillar_block.cpp
@@ -12,28 +12,15 @@ PillarBlock::ValidatorStakeChange::ValidatorStakeChange(const state_api::Validat
 PillarBlock::ValidatorStakeChange::ValidatorStakeChange(addr_t addr, dev::s256 stake_change)
     : addr_(addr), stake_change_(stake_change) {}
 
+RLP_FIELDS_DEFINE(PillarBlock::ValidatorStakeChange, addr_, stake_change_)
+
 PillarBlock::ValidatorStakeChange::ValidatorStakeChange(const dev::RLP& rlp) {
   util::rlp_tuple(util::RLPDecoderRef(rlp, true), addr_, stake_change_);
 }
 
-dev::bytes PillarBlock::ValidatorStakeChange::getRlp() const {
-  dev::RLPStream s(2);
-  s << addr_;
-  s << stake_change_;
-
-  return s.invalidate();
-}
-
 PillarBlock::PillarBlock(const dev::RLP& rlp) {
-  std::vector<dev::bytes> stakes_changes;
-  util::rlp_tuple(util::RLPDecoderRef(rlp, true), period_, state_root_, previous_pillar_block_hash_, stakes_changes);
-
-  validators_stakes_changes_.reserve(stakes_changes.size());
-  for (const auto& stake_change_bytes : stakes_changes) {
-    dev::RLP stake_change_rlp(stake_change_bytes);
-    validators_stakes_changes_.emplace_back(stake_change_rlp);
-  }
-
+  *this = util::rlp_dec<PillarBlock>(rlp);
+  // TODO: try if rlp.data().toBytes() produces the same output as getRlp()
   kCachedHash = dev::sha3(getRlp());
 }
 
@@ -46,22 +33,12 @@ PillarBlock::PillarBlock(PbftPeriod period, h256 state_root,
       previous_pillar_block_hash_(previous_pillar_block_hash),
       kCachedHash(dev::sha3(getRlp())) {}
 
-dev::bytes PillarBlock::getRlp() const {
-  dev::RLPStream s(4);
-  s << period_;
-  s << state_root_;
-  s << previous_pillar_block_hash_;
-
-  s.appendList(validators_stakes_changes_.size());
-  for (const auto& stake_change : validators_stakes_changes_) {
-    s.append(stake_change.getRlp());
-  }
-
-  return s.invalidate();
-}
+dev::bytes PillarBlock::getRlp() const { return util::rlp_enc(*this); }
 
 PbftPeriod PillarBlock::getPeriod() const { return period_; }
 
 PillarBlock::Hash PillarBlock::getHash() const { return kCachedHash; }
+
+RLP_FIELDS_DEFINE(PillarBlock, period_, state_root_, previous_pillar_block_hash_, validators_stakes_changes_)
 
 }  // namespace taraxa
