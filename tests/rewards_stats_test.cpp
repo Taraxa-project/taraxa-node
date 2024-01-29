c2549a5ffc37cb7c996b0eb50b79b5593abad3ed
Jakub Fornadel <jakub.fornadel@gmail.com>
2024-01-29 12:07:16 -0800

    post merge fixes

--- tests/rewards_stats_test.cpp
+++ tests/rewards_stats_test.cpp
@@ -21,7 +21,8 @@ class TestableRewardsStats : public rewards::Stats {
  public:
   TestableRewardsStats(const HardforksConfig::RewardsDistributionMap& rdm, std::shared_ptr<DB> db)
       : rewards::Stats(
-            100, HardforksConfig{0, {}, rdm, MagnoliaHardfork{0, 0}, AspenHardfork{0, 0}, FicusHardforkConfig{0, 0, 0}},
+            100,
+            HardforksConfig{0, 0, {}, rdm, MagnoliaHardfork{0, 0}, AspenHardfork{0, 0}, FicusHardforkConfig{0, 0, 0}},
             db, [](auto) { return 100; }) {}
   auto getStats() { return blocks_stats_; }
 };
