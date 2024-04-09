28c6dd42f7ca5371067222d09ed00b0d026d9c27
kstdl <kostello.d@gmail.com>
2024-04-09 14:59:02 +0200

    fix: pillar block encoding test

--- tests/pillar_chain_test.cpp
+++ tests/pillar_chain_test.cpp
@@ -278,7 +278,7 @@ TEST_F(PillarChainTest, block_serialization) {
     auto pb =
         pillar_chain::PillarBlock(11, h256(22), h256(33), std::move(validator_votes_count_changes), blk_hash_t(44));
     const auto bt = pb.encodeSolidity();
-    // TODO[2733]: how was this generated ???
+    // This hardcoded hex string is from bridge solidity test(TaraClient.t.sol/test_blockEncodeDecode)
     const auto expected = dev::jsToBytes(
         "00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000"
         "00000000000000000b00000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000"
@@ -301,9 +301,7 @@ TEST_F(PillarChainTest, block_serialization) {
     validator_votes_count_changes.emplace_back(addr_t(5), -5);
     validator_votes_count_changes.emplace_back(addr_t("0x290DEcD9548b62A8D60345A988386Fc84Ba6BC95"), 12151343246432);
     validator_votes_count_changes.emplace_back(addr_t("0xB10e2D527612073B26EeCDFD717e6a320cF44B4A"), -112321);
-    // TODO[2733]: 13534685468457923145 is too big, it needs to be within int32 max range
-    validator_votes_count_changes.emplace_back(addr_t("0x405787FA12A823e0F2b7631cc41B3bA8828b3321"),
-                                               -13534685468457923145);
+    validator_votes_count_changes.emplace_back(addr_t("0x405787FA12A823e0F2b7631cc41B3bA8828b3321"), -1353468546845791);
     validator_votes_count_changes.emplace_back(addr_t("0xc2575a0E9E593c00f959F8C92f12dB2869C3395a"), 9976987696);
     validator_votes_count_changes.emplace_back(addr_t("0x8a35AcfbC15Ff81A39Ae7d344fD709f28e8600B4"),
                                                465876798678065667);
@@ -311,7 +309,7 @@ TEST_F(PillarChainTest, block_serialization) {
     auto pb =
         pillar_chain::PillarBlock(11, h256(22), h256(33), std::move(validator_votes_count_changes), blk_hash_t(44));
     const auto bt = pb.encodeSolidity();
-    // TODO[2733]: how was this generated ???
+    // This hardcoded hex string is from bridge solidity test(TaraClient.t.sol/test_blockEncodeDecode)
     const auto expected = dev::jsToBytes(
         "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000"
         "0000000000000000000b000000000000000000000000000000000000000000000000000000000000001600000000000000000000000000"
@@ -326,7 +324,7 @@ TEST_F(PillarChainTest, block_serialization) {
         "00000000000000290decd9548b62a8d60345a988386fc84ba6bc9500000000000000000000000000000000000000000000000000000b0d"
         "347f6c60000000000000000000000000b10e2d527612073b26eecdfd717e6a320cf44b4affffffffffffffffffffffffffffffffffffff"
         "fffffffffffffffffffffe493f000000000000000000000000405787fa12a823e0f2b7631cc41b3ba8828b3321ffffffffffffffffffff"
-        "ffffffffffffffffffffffffffff442b2346b9ec8db7000000000000000000000000c2575a0e9e593c00f959f8c92f12db2869c3395a00"
+        "fffffffffffffffffffffffffffffffb31070c0cf3a1000000000000000000000000c2575a0e9e593c00f959f8c92f12db2869c3395a00"
         "00000000000000000000000000000000000000000000000000000252acc0300000000000000000000000008a35acfbc15ff81a39ae7d34"
         "4fd709f28e8600b40000000000000000000000000000000000000000000000000677207ae64d6203");
     ASSERT_EQ(bt, expected);
@@ -334,6 +332,8 @@ TEST_F(PillarChainTest, block_serialization) {
 }
 
 TEST_F(PillarChainTest, compact_signature) {
+  // This test cases are taken from https://eips.ethereum.org/EIPS/eip-2098
+
   // Private Key: 0x1234567890123456789012345678901234567890123456789012345678901234
   // Message: "Hello World"
   // Signature:
@@ -348,9 +348,8 @@ TEST_F(PillarChainTest, compact_signature) {
     ss.r = dev::h256("0x68a020a209d3d56c46f38cc50a33f704f4a9a10a59377f8dd762ac66910e9b90");
     ss.s = dev::h256("0x7e865ad05c4035ab5792787d4a0297a43617ae897930a6fe4d822b8faea52064");
     ss.v = 0;
-    // const auto compact_sig = dev::toCompact(sig);
     const auto compact_sig = dev::toCompact(ss);
-    // TODO[2733]: how was this generated ???
+    // compact sig = r + yParityAndS
     ASSERT_EQ(compact_sig, dev::h512("0x68a020a209d3d56c46f38cc50a33f704f4a9a10a59377f8dd762ac66910e9b907e865ad05c4035a"
                                      "b5792787d4a0297a43617ae897930a6fe4d822b8faea52064"));
   }
@@ -369,9 +368,8 @@ TEST_F(PillarChainTest, compact_signature) {
     ss.r = dev::h256("0x9328da16089fcba9bececa81663203989f2df5fe1faa6291a45381c81bd17f76");
     ss.s = dev::h256("0x139c6d6b623b42da56557e5e734a43dc83345ddfadec52cbe24d0cc64f550793");
     ss.v = 1;
-    // const auto compact_sig = dev::toCompact(sig);
     const auto compact_sig = dev::toCompact(ss);
-    // TODO[2733]: how was this generated ???
+    // compact sig = r + yParityAndS
     ASSERT_EQ(compact_sig, dev::h512("0x9328da16089fcba9bececa81663203989f2df5fe1faa6291a45381c81bd17f76939c6d6b623b42d"
                                      "a56557e5e734a43dc83345ddfadec52cbe24d0cc64f550793"));
   }
