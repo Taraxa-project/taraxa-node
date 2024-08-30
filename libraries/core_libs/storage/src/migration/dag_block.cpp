8f088dacf8e3fa9c1a4c6cbc8fcc8f15ddb11016
Matus Kysel <matuskysel1@gmail.com>
2024-08-30 11:37:19 +0200

    change to batches

--- libraries/core_libs/storage/src/migration/dag_block.cpp
+++ libraries/core_libs/storage/src/migration/dag_block.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 
 #include "common/thread_pool.hpp"
+#include "common/util.hpp"
 #include "pbft/period_data.hpp"
 
 namespace taraxa::storage::migration {
@@ -16,7 +17,7 @@ std::string DagBlockData::id() { return "DagBlockData"; }
 uint32_t DagBlockData::dbVersion() { return 1; }
 
 void DagBlockData::migrate(logger::Logger& log) {
-  auto orig_col = DB::Columns::period_data;
+  auto orig_col = DbStorage::Columns::period_data;
   auto copied_col = db_->copyColumn(db_->handle(orig_col), orig_col.name() + "-copy");
 
   if (copied_col == nullptr) {
@@ -24,7 +25,7 @@ void DagBlockData::migrate(logger::Logger& log) {
     return;
   }
 
-  auto it = db_->getColumnIterator(copied_col.get());
+  auto it = db_->getColumnIterator(orig_col);
   it->SeekToFirst();
   if (!it->Valid()) {
     return;
@@ -38,37 +39,31 @@ void DagBlockData::migrate(logger::Logger& log) {
     it->Prev();
   }
   memcpy(&end_period, it->key().data(), sizeof(uint64_t));
-  util::ThreadPool executor{std::thread::hardware_concurrency()};
-
   const auto diff = (end_period - start_period) ? (end_period - start_period) : 1;
   uint64_t curr_progress = 0;
-
+  auto batch = db_->createWriteBatch();
+  const size_t max_size = 500000000;
+  it->SeekToFirst();
   // Get and save data in new format for all blocks
-  for (uint64_t i = start_period; i <= end_period; ++i) {
-    executor.post([this, i, &copied_col]() {
-      const auto bytes = db_->getPeriodDataRaw(i);
-      const auto period_data_old_rlp = dev::RLP(bytes);
-      assert(period_data_old_rlp.itemCount() == 4);
+  for (; it->Valid(); it->Next()) {
+    uint64_t period;
+    memcpy(&period, it->key().data(), sizeof(uint64_t));
+    std::string raw = it->value().ToString();
+    const auto period_data_old_rlp = dev::RLP(raw);
+    auto period_data = ::taraxa::PeriodData::FromOldPeriodData(period_data_old_rlp);
+    db_->insert(batch, copied_col.get(), period, period_data.rlp());
 
-      auto period_data = ::taraxa::PeriodData::FromOldPeriodData(period_data_old_rlp);
-
-      db_->insert(copied_col.get(), i, period_data.rlp());
-    });
-    // This should slow down main loop so we are not using so much memory
-    while (executor.num_pending_tasks() > (executor.capacity() * 3)) {
-      taraxa::thisThreadSleepForMilliSeconds(50);
+    if (batch.GetDataSize() > max_size) {
+      db_->commitWriteBatch(batch);
     }
-    auto percentage = (i - start_period) * 100 / diff;
+
+    auto percentage = (period - start_period) * 100 / diff;
     if (percentage > curr_progress) {
       curr_progress = percentage;
       LOG(log) << "Migration " << id() << " progress " << curr_progress << "%";
     }
   }
-
-  // It's not perfect to check with sleep, but it's just migration that should be run once
-  do {
-    taraxa::thisThreadSleepForMilliSeconds(100);
-  } while (executor.num_pending_tasks());
+  db_->commitWriteBatch(batch);
 
   db_->replaceColumn(orig_col, std::move(copied_col));
 }
