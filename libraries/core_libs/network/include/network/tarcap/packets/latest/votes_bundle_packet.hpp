4cc60c53839c9fa756c0535e6af24f981f39d830
Jakub Fornadel <jakub.fornadel@gmail.com>
2024-10-29 14:44:31 +0100

    use optimized rlp format for votes bundles

--- libraries/core_libs/network/include/network/tarcap/packets/latest/votes_bundle_packet.hpp
+++ libraries/core_libs/network/include/network/tarcap/packets/latest/votes_bundle_packet.hpp
@@ -2,21 +2,15 @@
 
 #include "network/tarcap/packets_handlers/latest/common/exceptions.hpp"
 #include "vote/pbft_vote.hpp"
+#include "vote/votes_bundle_rlp.hpp"
 
 namespace taraxa::network::tarcap {
 
 struct VotesBundlePacket {
-  // TODO[2870]: Create votes bundles class
-  //  blk_hash_t votes_bundle_block_hash;
-  //  PbftPeriod votes_bundle_pbft_period;
-  //  PbftRound votes_bundle_pbft_round;
-  //  PbftStep votes_bundle_votes_step;
-
   std::vector<std::shared_ptr<PbftVote>> votes;
 
-  //  RLP_FIELDS_DEFINE_INPLACE(votes_bundle_block_hash, votes_bundle_pbft_period, votes_bundle_pbft_round,
-  //                            votes_bundle_votes_step, votes)
-  RLP_FIELDS_DEFINE_INPLACE(votes)
+  void rlp(::taraxa::util::RLPDecoderRef encoding) { votes = decodePbftVotesBundleRlp(encoding.value); }
+  void rlp(::taraxa::util::RLPEncoderRef encoding) const { encoding.appendRaw(encodePbftVotesBundleRlp(votes)); }
 };
 
 }  // namespace taraxa::network::tarcap
