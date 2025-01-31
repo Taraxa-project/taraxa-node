ce0ae1259b729748db4b1a1c9ec3c264a088c6ee
kstdl <kostello.d@gmail.com>
2025-01-31 20:57:21 +0100

    perf: fix unneeded period data deserizalization/serialization

--- libraries/core_libs/network/include/network/tarcap/packets/latest/pbft_sync_packet.hpp
+++ libraries/core_libs/network/include/network/tarcap/packets/latest/pbft_sync_packet.hpp
@@ -14,4 +14,17 @@ struct PbftSyncPacket {
   RLP_FIELDS_DEFINE_INPLACE(last_block, period_data, current_block_cert_votes_bundle)
 };
 
+struct PbftSyncPacketRaw {
+  bool last_block;
+  dev::bytes period_data;
+  std::optional<OptimizedPbftVotesBundle> current_block_cert_votes_bundle;
+
+  void rlp(::taraxa::util::RLPEncoderRef encoding) const {
+    encoding.appendList(3);
+    encoding.append(last_block);
+    encoding.appendRaw(period_data);
+    util::rlp(encoding, current_block_cert_votes_bundle);
+  }
+};
+
 }  // namespace taraxa::network::tarcap
