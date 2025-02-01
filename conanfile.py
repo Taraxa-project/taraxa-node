39d16de542f1b00d29ee796b7661c4830696c2e4
kstdl <kostello.d@gmail.com>
2025-02-01 17:06:33 +0100

    chore: update rocksdb version

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class TaraxaConan(ConanFile):
         self.requires("cryptopp/8.9.0")
         self.requires("gtest/1.15.0")
         self.requires("lz4/1.10.0")
-        self.requires("rocksdb/9.2.1")
+        self.requires("rocksdb/9.7.3")
         self.requires("prometheus-cpp/1.2.4")
         self.requires("jsoncpp/1.9.5")
 
