729cebb15c59824455edb2cae9204ffa1e423c3c
kstdl <kostello.d@gmail.com>
2025-02-26 15:49:05 +0100

    feat: make receipts migration optional

--- libraries/app/src/app.cpp
+++ libraries/app/src/app.cpp
@@ -97,6 +97,10 @@ void App::init(const cli::Config &cli_conf) {
 
     auto migration_manager = storage::migration::Manager(db_);
     migration_manager.applyAll();
+
+    if (conf_.db_config.migrate_receipts_by_period) {
+      migration_manager.applyReceiptsByPeriod();
+    }
     if (db_->getDagBlocksCount() == 0) {
       db_->setGenesisHash(conf_.genesis.genesisHash());
     }
@@ -313,8 +317,8 @@ void App::rebuildDb() {
       }
     } else {
       next_period_data = std::make_shared<PeriodData>(std::move(data));
-      // More efficient to get sender(which is expensive) on this thread which is not as busy as the thread that pushes
-      // blocks to chain
+      // More efficient to get sender(which is expensive) on this thread which is not as busy as the thread that
+      // pushes blocks to chain
       for (auto &t : next_period_data->transactions) t->getSender();
       cert_votes = next_period_data->previous_block_cert_votes;
     }
