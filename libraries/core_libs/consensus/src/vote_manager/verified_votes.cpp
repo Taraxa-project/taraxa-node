ef1eb7892418b2d186d905d3a9cb60efeee1ccf8
kstdl <kostello.d@gmail.com>
2025-07-23 12:35:33 +0200

    fix: verified votes haning

--- libraries/core_libs/consensus/src/vote_manager/verified_votes.cpp
+++ libraries/core_libs/consensus/src/vote_manager/verified_votes.cpp
@@ -41,22 +41,46 @@ std::vector<std::shared_ptr<PbftVote>> VerifiedVotes::votes() const {
 }
 
 std::optional<const RoundVerifiedVotesMap> VerifiedVotes::getPeriodVotes(PbftPeriod period) const {
-  if (auto [found, it] = get(period); found) {
-    return it->second;
+  std::shared_lock lock(verified_votes_access_);
+
+  auto period_it = verified_votes_.find(period);
+  if (period_it != verified_votes_.end()) {
+    return period_it->second;
   }
   return std::nullopt;
 }
 
 std::optional<const RoundVerifiedVotes> VerifiedVotes::getRoundVotes(PbftPeriod period, PbftRound round) const {
-  if (auto [found, it] = get(period, round); found) {
-    return it->second;
+  std::shared_lock lock(verified_votes_access_);
+
+  auto period_it = verified_votes_.find(period);
+  if (period_it == verified_votes_.end()) {
+    return std::nullopt;
+  }
+
+  auto round_it = period_it->second.find(round);
+  if (round_it != period_it->second.end()) {
+    return round_it->second;
   }
   return std::nullopt;
 }
 
 std::optional<const StepVotes> VerifiedVotes::getStepVotes(PbftPeriod period, PbftRound round, PbftStep step) const {
-  if (auto [found, it] = get(period, round, step); found) {
-    return it->second;
+  std::shared_lock lock(verified_votes_access_);
+
+  auto period_it = verified_votes_.find(period);
+  if (period_it == verified_votes_.end()) {
+    return std::nullopt;
+  }
+
+  auto round_it = period_it->second.find(round);
+  if (round_it == period_it->second.end()) {
+    return std::nullopt;
+  }
+
+  auto step_it = round_it->second.step_votes.find(step);
+  if (step_it != round_it->second.step_votes.end()) {
+    return step_it->second;
   }
   return std::nullopt;
 }
@@ -112,36 +136,60 @@ std::vector<std::shared_ptr<PbftVote>> VerifiedVotes::getTwoTPlusOneVotedBlockVo
 }
 
 void VerifiedVotes::setNetworkTPlusOneStep(std::shared_ptr<PbftVote> vote) {
-  auto [found, round_it] = get(vote->getPeriod(), vote->getRound());
-  if (!found) {
+  std::scoped_lock lock(verified_votes_access_);
+
+  const auto found_period_it = verified_votes_.find(vote->getPeriod());
+  if (found_period_it == verified_votes_.end()) {
     return;
   }
 
-  std::scoped_lock lock(verified_votes_access_);
-  round_it->second.network_t_plus_one_step = vote->getStep();
+  const auto found_round_it = found_period_it->second.find(vote->getRound());
+  if (found_round_it == found_period_it->second.end()) {
+    return;
+  }
+
+  found_round_it->second.network_t_plus_one_step = vote->getStep();
 }
 
 void VerifiedVotes::insertTwoTPlusOneVotedBlock(TwoTPlusOneVotedBlockType type, std::shared_ptr<PbftVote> vote) {
-  auto [found, round_it] = get(vote->getPeriod(), vote->getRound());
-  if (!found) {
+  std::scoped_lock lock(verified_votes_access_);
+
+  const auto found_period_it = verified_votes_.find(vote->getPeriod());
+  if (found_period_it == verified_votes_.end()) {
     std::cerr << "insertTwoTPlusOneVotedBlock: period " << vote->getPeriod() << ", round " << vote->getRound()
               << std::endl;
     return;
   }
 
-  std::scoped_lock lock(verified_votes_access_);
-  round_it->second.two_t_plus_one_voted_blocks_.emplace(type, VotedBlock{vote->getBlockHash(), vote->getStep()});
+  const auto found_round_it = found_period_it->second.find(vote->getRound());
+  if (found_round_it == found_period_it->second.end()) {
+    std::cerr << "insertTwoTPlusOneVotedBlock: period " << vote->getPeriod() << ", round " << vote->getRound()
+              << std::endl;
+    return;
+  }
+
+  found_round_it->second.two_t_plus_one_voted_blocks_.emplace(type, VotedBlock{vote->getBlockHash(), vote->getStep()});
 }
 
 std::optional<std::shared_ptr<PbftVote>> VerifiedVotes::insertUniqueVoter(const std::shared_ptr<PbftVote>& vote) {
-  auto [found, step_it] = get(vote->getPeriod(), vote->getRound(), vote->getStep());
+  std::scoped_lock lock(verified_votes_access_);
 
-  if (!found) {
+  const auto found_period_it = verified_votes_.find(vote->getPeriod());
+  if (found_period_it == verified_votes_.end()) {
     return std::nullopt;
   }
 
-  std::scoped_lock lock(verified_votes_access_);
-  auto inserted_vote = step_it->second.unique_voters.insert({vote->getVoterAddr(), {vote, nullptr}});
+  const auto found_round_it = found_period_it->second.find(vote->getRound());
+  if (found_round_it == found_period_it->second.end()) {
+    return std::nullopt;
+  }
+
+  const auto found_step_it = found_round_it->second.step_votes.find(vote->getStep());
+  if (found_step_it == found_round_it->second.step_votes.end()) {
+    return std::nullopt;
+  }
+
+  auto inserted_vote = found_step_it->second.unique_voters.insert({vote->getVoterAddr(), {vote, nullptr}});
 
   // Vote was successfully inserted -> it is unique
   if (inserted_vote.second) {
@@ -245,61 +293,6 @@ void VerifiedVotes::cleanupVotesByPeriod(PbftPeriod pbft_period) {
   while (it != verified_votes_.end() && it->first < pbft_period) {
     it = verified_votes_.erase(it);
   }
-}
-
-template <typename IteratorType, typename MapType>
-std::pair<bool, IteratorType> getImpl(PbftPeriod period, MapType& map) {
-  auto it = map.find(period);
-  return {it != map.end(), it};
-}
-
-std::pair<bool, PeriodVerifiedVotesMap::iterator> VerifiedVotes::get(PbftPeriod period) {
-  std::shared_lock lock(verified_votes_access_);
-  return getImpl<PeriodVerifiedVotesMap::iterator>(period, verified_votes_);
-}
-
-std::pair<bool, PeriodVerifiedVotesMap::const_iterator> VerifiedVotes::get(PbftPeriod period) const {
-  std::shared_lock lock(verified_votes_access_);
-  return getImpl<PeriodVerifiedVotesMap::const_iterator>(period, verified_votes_);
-}
-
-std::pair<bool, RoundVerifiedVotesMap::iterator> VerifiedVotes::get(PbftPeriod period, PbftRound round) {
-  std::shared_lock lock(verified_votes_access_);
-  auto res = get(period);
-  if (!res.first) {
-    return {false, RoundVerifiedVotesMap::iterator{}};
-  }
-  return getImpl<RoundVerifiedVotesMap::iterator>(round, res.second->second);
-}
-
-std::pair<bool, RoundVerifiedVotesMap::const_iterator> VerifiedVotes::get(PbftPeriod period, PbftRound round) const {
-  std::shared_lock lock(verified_votes_access_);
-  auto res = get(period);
-  if (!res.first) {
-    return {false, RoundVerifiedVotesMap::const_iterator{}};
-  }
-  return getImpl<RoundVerifiedVotesMap::const_iterator>(round, res.second->second);
-}
-
-std::pair<bool, StepVotesMap::iterator> VerifiedVotes::get(PbftPeriod period, PbftRound round, PbftStep step) {
-  std::shared_lock lock(verified_votes_access_);
-  auto it = get(period, round);
-  if (!it.first) {
-    return {false, StepVotesMap::iterator{}};
-  }
-  auto step_it = it.second->second.step_votes.find(step);
-  return {step_it != it.second->second.step_votes.end(), step_it};
-}
-
-std::pair<bool, StepVotesMap::const_iterator> VerifiedVotes::get(PbftPeriod period, PbftRound round,
-                                                                 PbftStep step) const {
-  std::shared_lock lock(verified_votes_access_);
-  auto it = get(period, round);
-  if (!it.first) {
-    return {false, StepVotesMap::const_iterator{}};
-  }
-  auto step_it = it.second->second.step_votes.find(step);
-  return {step_it != it.second->second.step_votes.end(), step_it};
 }
 
 }  // namespace taraxa
