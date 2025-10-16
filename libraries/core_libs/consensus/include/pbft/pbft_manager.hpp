0572942320750d018a830f5e0928eaddc565a0c8
Jakub Fornadel <jakub.fornadel@gmail.com>
2025-10-16 19:07:04 +0200

    Cleanup old lambda related code

--- libraries/core_libs/consensus/include/pbft/pbft_manager.hpp
+++ libraries/core_libs/consensus/include/pbft/pbft_manager.hpp
@@ -226,12 +226,6 @@ class PbftManager {
   blk_hash_t lastPbftBlockHashFromQueueOrChain();
 
   /**
-   * @brief Get PBFT lambda. PBFT lambda is a timer clock
-   * @return PBFT lambda
-   */
-  std::chrono::milliseconds getPbftInitialLambda() const { return kMinLambda; }
-
-  /**
    * @brief Calculate DAG blocks ordering hash
    * @param dag_block_hashes DAG blocks hashes
    * @return DAG blocks ordering hash
@@ -605,7 +599,7 @@ class PbftManager {
 
   /**
    * @param period
-   * @return pbft deadline time - max time to dinalize the block in provided period
+   * @return pbft deadline time - max time to finalize the block in provided period
    */
   std::chrono::milliseconds getPbftDeadline(PbftPeriod period) const;
 
@@ -633,14 +627,12 @@ class PbftManager {
   std::shared_ptr<final_chain::FinalChain> final_chain_;
   std::shared_ptr<pillar_chain::PillarChainManager> pillar_chain_mgr_;
 
-  // TODO: remove kMinLambda, kGenesisConfig as kConfig can be used instead
   const FullNodeConfig kConfig;
   const uint32_t kSyncingThreadPoolSize;
   std::shared_ptr<util::ThreadPool>
       sync_thread_pool_;  // Thread pool used for transaction sender retrieval in syncing blocks
 
-  const std::chrono::milliseconds kMinLambda;         // [ms]
-  const std::chrono::milliseconds kMaxLambda{60000};  // in ms, max lambda is 1 minute
+  const std::chrono::milliseconds kMaxExponentialLambda{60000};  // [ms], max lambda is 1 minute
 
   uint32_t rounds_count_dynamic_lambda_{0};  // rounds count per cacti_hf.lambda_change_interval blocks
   uint32_t dynamic_lambda_{0};               // [ms] - dynamic lambda that can be anywhere between <500ms, 1500ms>
